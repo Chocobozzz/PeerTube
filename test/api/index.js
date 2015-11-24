2dd5b570a6bb774eda2fb9b9ce7cffd0741eb0a2
Chocobozzz <florian.bigard@gmail.com>
2015-11-24 09:13:42 +0100

    Force the mocha tests order

---
+++ test/api/index.js
@@ -0,0 +1,10 @@
+;(function () {
+  'use strict'
+
+  // Order of the tests we want to execute
+  require('./checkParams')
+  require('./friendsBasic')
+  require('./singlePod')
+  require('./multiplePods')
+  require('./friendsAdvanced')
+})()
---
+++ test/index.js
@@ -0,0 +1,6 @@
+;(function () {
+  'use strict'
+
+  // Order of the tests we want to execute
+  require('./api/')
+})()
