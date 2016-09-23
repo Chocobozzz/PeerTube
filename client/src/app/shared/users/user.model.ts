44c5275e1bdd1d4b4c23f7c423034e4293c927b3
Chocobozzz <florian.bigard@gmail.com>
2016-09-23 17:34:18 +0200

    Client: fix compilation about the user created date

--- client/src/app/shared/users/user.model.ts
+++ client/src/app/shared/users/user.model.ts
@@ -4,11 +4,14 @@ export class User {
   role: string;
   createdDate: Date;
 
-  constructor(hash: { id: string, username: string, role: string, createdDate: Date }) {
+  constructor(hash: { id: string, username: string, role: string, createdDate?: Date }) {
     this.id = hash.id;
     this.username = hash.username;
     this.role = hash.role;
-    this.createdDate = hash.createdDate;
+
+    if (hash.createdDate) {
+      this.createdDate = hash.createdDate;
+    }
   }
 
   isAdmin() {
