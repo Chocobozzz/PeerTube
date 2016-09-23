b539c9b34c246eb7bfc4afea92ae095cb1c38e59
Chocobozzz <florian.bigard@gmail.com>
2016-09-23 17:20:15 +0200

    Client: display created user date

--- client/src/app/shared/users/user.model.ts
+++ client/src/app/shared/users/user.model.ts
@@ -2,11 +2,13 @@ export class User {
   id: string;
   username: string;
   role: string;
+  createdDate: Date;
 
-  constructor(hash: { id: string, username: string, role: string }) {
+  constructor(hash: { id: string, username: string, role: string, createdDate: Date }) {
     this.id = hash.id;
     this.username = hash.username;
     this.role = hash.role;
+    this.createdDate = hash.createdDate;
   }
 
   isAdmin() {
