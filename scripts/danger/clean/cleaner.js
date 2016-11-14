c92372d0aa8bd2bc02152941cfd4546b45949368
Chocobozzz <florian.bigard@gmail.com>
2016-11-14 22:46:04 +0100

    Server: fix db clean for clean script

--- scripts/danger/clean/cleaner.js
+++ scripts/danger/clean/cleaner.js
@@ -4,7 +4,7 @@ mongoose.Promise = global.Promise
 
 const constants = require('../../../server/initializers/constants')
 
-const mongodbUrl = 'mongodb://' + constants.CONFIG.DATABASE.HOST + ':' + constants.CONFIG.DATABASE.PORT + '/' + constants.CONFIG.DATABASE.DBNAME
+const mongodbUrl = 'mongodb://' + constants.CONFIG.DATABASE.HOSTNAME + ':' + constants.CONFIG.DATABASE.PORT + '/' + constants.CONFIG.DATABASE.DBNAME
 mongoose.connect(mongodbUrl, function () {
   console.info('Deleting MongoDB %s database.', constants.CONFIG.DATABASE.DBNAME)
   mongoose.connection.dropDatabase(function () {
