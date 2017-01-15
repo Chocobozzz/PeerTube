7df5e5e4b1602ce412b310dd88c83bac937d0a35
Chocobozzz <florian.bigard@gmail.com>
2017-01-15 10:05:53 +0100

    Server: add script that reset the password of a user

---
+++ scripts/reset-password.js
@@ -0,0 +1,45 @@
+#!/usr/bin/env node
+
+'use strict'
+
+// TODO: document this script
+
+const program = require('commander')
+
+const constants = require('../server/initializers/constants')
+const db = require('../server/initializers/database')
+
+program
+  .option('-u, --user [user]', 'User')
+  .option('-p, --password [new password]', 'New password')
+  .parse(process.argv)
+
+if (program.user === undefined || program.password === undefined) {
+  console.error('All parameters are mandatory.')
+  process.exit(-1)
+}
+
+db.init(true, function () {
+  db.User.loadByUsername(program.user, function (err, user) {
+    if (err) {
+      console.error(err)
+      return
+    }
+
+    if (!user) {
+      console.error('User unknown.')
+      return
+    }
+
+    user.password = program.password
+    user.save().asCallback(function (err) {
+      if (err) {
+        console.error(err)
+        return
+      }
+
+      console.log('User pasword updated.')
+      process.exit(0)
+    })
+  })
+})
