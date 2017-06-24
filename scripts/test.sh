743cb5e5c44c4ba97a877e9a172adabd666c9852
Chocobozzz <florian.bigard@gmail.com>
2017-06-24 10:37:43 +0200

    Stop mocha tests after a failure

--- scripts/test.sh
+++ scripts/test.sh
@@ -5,4 +5,4 @@ npm test || exit -1
 
 cd .. || exit -1
 npm run tslint -- --type-check --project ./tsconfig.json -c ./tslint.json server.ts server/**/*.ts || exit -1
-mocha server/tests
+mocha --bail server/tests
