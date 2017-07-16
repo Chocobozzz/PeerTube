ec6702b9ce63045e8ba30275070e0629121df636
Green-Star <alexandre.sahuc@cpy.re>
2017-07-16 18:57:02 +0200

    Add check-params remove blacklisted video tests

--- server/tests/api/check-params/video-blacklists.ts
+++ server/tests/api/check-params/video-blacklists.ts
@@ -1,6 +1,7 @@
 /* tslint:disable:no-unused-expression */
 
 import 'mocha'
+import * as request from 'supertest'
 
 import {
   ServerInfo,
@@ -76,6 +77,51 @@ describe('Test video blacklists API validators', function () {
       const fields = {}
       const path = basePath + server.video.id + '/blacklist'
       await makePostBodyRequest({ url: server.url, path, token: server.accessToken, fields, statusCodeExpected: 403 })
+    })
+  })
+
+  describe('When removing a video in blacklist', function () {
+    const basePath = '/api/v1/blacklists/'
+
+    it('Should fail with a non authenticated user', async function () {
+      const path = basePath + server.video.id
+
+      await request(server.url)
+              .delete(path)
+              .set('Authorization', 'Bearer ' + 'fake token')
+              .set('Accept', 'application/json')
+              .expect(401)
+    })
+
+    it('Should fail with a non admin user', async function () {
+      const path = basePath + server.video.id
+
+      await request(server.url)
+              .delete(path)
+              .set('Authorization', 'Bearer ' + userAccessToken)
+              .set('Accept', 'application/json')
+              .expect(403)
+    })
+
+    it('Should fail with an incorrect id', async function () {
+      const path = basePath + 'foobar'
+
+      await request(server.url)
+              .delete(path)
+              .set('Authorization', 'Bearer ' + server.accessToken)
+              .set('Accept', 'application/json')
+              .expect(400)
+    })
+
+    it('Should fail with a not blacklisted video', async function () {
+      // The video was not added to the blacklist so it should fail
+      const path = basePath + server.video.id
+
+      await request(server.url)
+              .delete(path)
+              .set('Authorization', 'Bearer ' + server.accessToken)
+              .set('Accept', 'application/json')
+              .expect(404)
     })
   })
 
