36fcb02c67c7d93c994cee2b9bb5efcbe3e82540
Green-Star <alexandre.sahuc@cpy.re>
2017-08-19 14:12:27 +0200

    Add list blacklist tests

--- server/tests/utils/video-blacklists.ts
+++ server/tests/utils/video-blacklists.ts
@@ -10,8 +10,34 @@ function addVideoToBlacklist (url: string, token: string, videoId: number, speci
           .expect(specialStatus)
 }
 
+function getBlacklistedVideosList (url: string, token: string, specialStatus = 200) {
+  const path = '/api/v1/blacklists/'
+
+  request(url)
+    .get(path)
+    .query({ sort: 'createdAt' })
+    .set('Accept', 'application/json')
+    .set('Authorization', 'Bearer ' + token)
+    .expect(specialStatus)
+    .expect('Content-Type', /json/)
+}
+
+function getSortedBlacklistedVideosList (url: string, token: string, sort: string, specialStatus = 200) {
+  const path = '/api/v1/blacklists/'
+
+  request(url)
+    .get(path)
+    .query({ sort: sort })
+    .set('Accept', 'application/json')
+    .set('Authorization', 'Bearer ' + token)
+    .expect(specialStatus)
+    .expect('Content-Type', /json/)
+}
+
 // ---------------------------------------------------------------------------
 
 export {
-  addVideoToBlacklist
+  addVideoToBlacklist,
+  getBlacklistedVideosList,
+  getSortedBlacklistedVideosList
 }
