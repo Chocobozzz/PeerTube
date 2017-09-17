19adbb719f4ea77cf11a66bd127aa33581ff8708
Green-Star <alexandre.sahuc@cpy.re>
2017-09-17 19:37:23 +0200

    Server: Use new checkParams methods

--- server/middlewares/validators/blacklist.ts
+++ server/middlewares/validators/blacklist.ts
@@ -1,30 +1,32 @@
-import 'express-validator'
+import { param } from 'express-validator/check'
 import * as express from 'express'
 
 import { database as db } from '../../initializers/database'
 import { checkErrors } from './utils'
 import { logger } from '../../helpers'
 
-function blacklistRemoveValidator (req: express.Request, res: express.Response, next: express.NextFunction) {
-  req.checkParams('id', 'Should have a valid id').notEmpty().isNumeric()
+const blacklistRemoveValidator = [
+  param('id').isNumeric().not().isEmpty().withMessage('Should have a valid id'),
 
-  logger.debug('Checking blacklistRemove parameters.', { parameters: req.params })
+  (req: express.Request, res: express.Response, next: express.NextFunction) => {
+    logger.debug('Checking blacklistRemove parameters.', { parameters: req.params })
 
-  checkErrors(req, res, function () {
-    db.BlacklistedVideo.loadById(req.params.id)
-      .then(entry => {
-        if (!entry) return res.status(404).send('Blacklisted video not found')
+    checkErrors(req, res, () => {
+      db.BlacklistedVideo.loadById(req.params.id)
+        .then(entry => {
+          if (!entry) return res.status(404).send('Blacklisted video not found')
 
-        res.locals.blacklistEntryToRemove = entry
+          res.locals.blacklistEntryToRemove = entry
 
-        next()
-      })
-      .catch(err => {
-        logger.error('Error in blacklistRemove request validator', { error: err })
-        return res.sendStatus(500)
-      })
-  })
-}
+          next()
+        })
+        .catch(err => {
+          logger.error('Error in blacklistRemove request validator', { error: err })
+          return res.sendStatus(500)
+        })
+    })
+  }
+]
 
 // ---------------------------------------------------------------------------
 
