e600e1fea275c12f4420e23624804617e61a082c
Chocobozzz <florian.bigard@gmail.com>
2017-12-08 15:22:57 +0100

    Design follow admin page

--- client/src/app/shared/forms/form-validators/host.validator.ts
+++ client/src/app/shared/forms/form-validators/host.validator.ts
@@ -1,14 +1,8 @@
-import { FormControl } from '@angular/forms'
-
-export function validateHost (c: FormControl) {
+export function validateHost (value: string) {
   // Thanks to http://stackoverflow.com/a/106223
   const HOST_REGEXP = new RegExp(
     '^(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\-]*[A-Za-z0-9])$'
   )
 
-  return HOST_REGEXP.test(c.value) ? null : {
-    validateHost: {
-      valid: false
-    }
-  }
+  return HOST_REGEXP.test(value)
 }
