fb4fd623d5e5adcfdc9ecf3dffef702b3786f486
Chocobozzz <me@florianbigard.com>
2017-12-21 15:24:49 +0100

    Better admin tables

--- client/src/app/videos/+video-watch/video-watch.module.ts
+++ client/src/app/videos/+video-watch/video-watch.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core'
-
-import { VideoWatchRoutingModule } from './video-watch-routing.module'
-import { MarkdownService } from '../shared'
-import { SharedModule } from '../../shared'
+import { TooltipModule } from 'ngx-bootstrap/tooltip'
 import { ClipboardModule } from 'ngx-clipboard'
-import { TooltipModule } from 'ngx-bootstrap/tooltip';
-
-import { VideoWatchComponent } from './video-watch.component'
+import { SharedModule } from '../../shared'
+import { MarkdownService } from '../shared'
+import { VideoDownloadComponent } from './video-download.component'
 import { VideoReportComponent } from './video-report.component'
 import { VideoShareComponent } from './video-share.component'
-import { VideoDownloadComponent } from './video-download.component'
+
+import { VideoWatchRoutingModule } from './video-watch-routing.module'
+
+import { VideoWatchComponent } from './video-watch.component'
 
 @NgModule({
   imports: [
