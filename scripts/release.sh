1e5c707b08b0cb8b61138e583747ec0ce681547b
Chocobozzz <me@florianbigard.com>
2018-01-15 13:23:48 +0100

    Add github release to release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -16,20 +16,42 @@ if [ -z "$1" ]; then
   exit -1
 fi
 
+if [ -z $GITHUB_TOKEN ]; then
+  echo "Need GITHUB_TOKEN env set."
+  exit -1
+fi
+
+version="v$1"
+directory_name="peertube-$version"
+zip_name="peertube-$version.zip"
+
 cd ./client || exit -1
 npm version --no-git-tag-version --no-commit-hooks $1 || exit -1
 
 cd ../ || exit -1
 npm version -f --no-git-tag-version --no-commit-hooks $1 || exit -1
 
-git commit package.json client/package.json -m "Bumped to version $1" || exit -1
-git tag -s -a "v$1" -m "v$1"
+git commit package.json client/package.json -m "Bumped to version $version" || exit -1
+git tag -s -a "$version" -m "$version"
 
 npm run build || exit -1
-#npm test || exit -1
 
 cd ../ || exit -1
 
-ln -s "PeerTube" "peertube-v$1" || exit -1
-zip -r "PeerTube/peertube-v$1.zip" "peertube-v$1"/{CREDITS.md,FAQ.md,LICENSE,README.md,client/dist/,client/yarn.lock,client/package.json,config,dist,package.json,scripts,support,tsconfig.json,yarn.lock} || exit -1
-rm "peertube-v$1" || exit -1
+ln -s "PeerTube" "$directory_name" || exit -1
+zip -r "PeerTube/$zip_name" "$directory_name/CREDITS.md" "$directory_name/FAQ.md" \
+                            "$directory_name/LICENSE" "$directory_name/README.md" \
+                            "$directory_name/client/dist/" "$directory_name/client/yarn.lock" \
+                            "$directory_name/client/package.json" "$directory_name/config" \
+                            "$directory_name/dist" "$directory_name/package.json" \
+                            "$directory_name/scripts" "$directory_name/support" \
+                            "$directory_name/tsconfig.json" "$directory_name/yarn.lock" \
+                            || exit -1
+rm "$directory_name" || exit -1
+
+cd "PeerTube" || exit -1
+
+git push origin --tag
+
+github-release release --user chocobozzz --repo peertube --tag "$version" --name "$version"
+github-release upload --user chocobozzz --repo peertube --tag "$version" --name "$version" --file "$zip_name"
