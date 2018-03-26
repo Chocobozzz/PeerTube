53eb90c0ab584ee48774e15ee7c0e764cfe4559d
Chocobozzz <me@florianbigard.com>
2018-03-26 09:48:10 +0200

    Fix check comments params unit tests

--- client/src/app/shared/forms/form-validators/video-comment.ts
+++ client/src/app/shared/forms/form-validators/video-comment.ts
@@ -1,7 +1,7 @@
 import { Validators } from '@angular/forms'
 
 export const VIDEO_COMMENT_TEXT = {
-  VALIDATORS: [ Validators.required, Validators.minLength(2), Validators.maxLength(3000) ],
+  VALIDATORS: [ Validators.required, Validators.minLength(1), Validators.maxLength(3000) ],
   MESSAGES: {
     'required': 'Comment is required.',
     'minlength': 'Comment must be at least 2 characters long.',
