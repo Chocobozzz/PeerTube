4aeb67a569b30a233dcff2eaf9c564d62ccd6497
Chocobozzz <me@florianbigard.com>
2018-04-16 18:44:57 +0200

    Improve dockerfile

--- support/docker/production/docker-entrypoint.sh
+++ support/docker/production/docker-entrypoint.sh
@@ -4,13 +4,13 @@ set -e
 # first arg is `-f` or `--some-option`
 # or first arg is `something.conf`
 if [ "${1#-}" != "$1" ] || [ "${1%.conf}" != "$1" ]; then
-        set -- npm "$@"
+    set -- npm "$@"
 fi
 
 # allow the container to be started with `--user`
-if [ "$1" = 'npm' -a "$(id -u)" = '0' ]; then
-        chown -R peertube: /data
-        exec gosu peertube "$0" "$@"
+if [ "$1" = 'npm' -a "$(id -u)" != '0' ]; then
+    chown -R peertube:peertube /data
+    exec gosu peertube "$0" "$@"
 fi
 
 exec "$@"
