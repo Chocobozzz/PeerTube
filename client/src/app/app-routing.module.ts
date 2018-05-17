4b4f22fc30063bb715230b1e0f76ac25050c5a55
Chocobozzz <me@florianbigard.com>
2018-05-17 15:25:50 +0200

    Remove bad comments

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -4,22 +4,22 @@ import { RouterModule, Routes } from '@angular/router'
 import { PreloadSelectedModulesList } from './core'
 
 const routes: Routes = [
-  // {
-  //   path: 'admin',
-  //   loadChildren: './+admin/admin.module#AdminModule'
-  // },
-  // {
-  //   path: 'my-account',
-  //   loadChildren: './+my-account/my-account.module#MyAccountModule'
-  // },
-  // {
-  //   path: 'accounts',
-  //   loadChildren: './+accounts/accounts.module#AccountsModule'
-  // },
-  // {
-  //   path: 'video-channels',
-  //   loadChildren: './+video-channels/video-channels.module#VideoChannelsModule'
-  // }
+  {
+    path: 'admin',
+    loadChildren: './+admin/admin.module#AdminModule'
+  },
+  {
+    path: 'my-account',
+    loadChildren: './+my-account/my-account.module#MyAccountModule'
+  },
+  {
+    path: 'accounts',
+    loadChildren: './+accounts/accounts.module#AccountsModule'
+  },
+  {
+    path: 'video-channels',
+    loadChildren: './+video-channels/video-channels.module#VideoChannelsModule'
+  }
 ]
 
 @NgModule({
