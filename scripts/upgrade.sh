ee1219d83d4d3c6c59236c2e854e6b3ff36fa30f
Jocelyn Jaubert <jocelyn.jaubert@gmail.com>
2018-05-21 21:48:10 +0200

    Move /var/www/peertube to $PEERTUBE_PATH in upgrade.sh

--- scripts/upgrade.sh
+++ scripts/upgrade.sh
@@ -2,26 +2,28 @@
 
 set -eu
 
+PEERTUBE_PATH=/var/www/peertube/
+
 # Backup database
-SQL_BACKUP_PATH="/var/www/peertube/backup/sql-peertube_prod-$(date +"%Y%m%d-%H%M").bak" 
-mkdir -p /var/www/peertube/backup
+SQL_BACKUP_PATH="$PEERTUBE_PATH/backup/sql-peertube_prod-$(date +"%Y%m%d-%H%M").bak" 
+mkdir -p $PEERTUBE_PATH/backup
 pg_dump -U peertube -W -h localhost -F c peertube_prod -f "$SQL_BACKUP_PATH" 
 
 # Get and Display the Latest Version
 VERSION=$(curl -s https://api.github.com/repos/chocobozzz/peertube/releases/latest | grep tag_name | cut -d '"' -f 4)
 echo "Latest Peertube version is $VERSION"
-wget -q "https://github.com/Chocobozzz/PeerTube/releases/download/${VERSION}/peertube-${VERSION}.zip" -O "/var/www/peertube/versions/peertube-${VERSION}.zip"
-cd /var/www/peertube/versions
+wget -q "https://github.com/Chocobozzz/PeerTube/releases/download/${VERSION}/peertube-${VERSION}.zip" -O "$PEERTUBE_PATH/versions/peertube-${VERSION}.zip"
+cd $PEERTUBE_PATH/versions
 unzip -o "peertube-${VERSION}.zip"
 rm -f "peertube-${VERSION}.zip"
 
 # Upgrade Scripts
-rm -rf /var/www/peertube/peertube-latest
-ln -s "/var/www/peertube/versions/peertube-${VERSION}" /var/www/peertube/peertube-latest
-cd /var/www/peertube/peertube-latest
+rm -rf $PEERTUBE_PATH/peertube-latest
+ln -s "$PEERTUBE_PATH/versions/peertube-${VERSION}" $PEERTUBE_PATH/peertube-latest
+cd $PEERTUBE_PATH/peertube-latest
 yarn install --production --pure-lockfile 
-cp /var/www/peertube/peertube-latest/config/default.yaml /var/www/peertube/config/default.yaml
+cp $PEERTUBE_PATH/peertube-latest/config/default.yaml $PEERTUBE_PATH/config/default.yaml
 
 echo "Differences in configuration files..."
-diff "/var/www/peertube/versions/peertube-${VERSION}/config/production.yaml.example" /var/www/peertube/config/production.yaml
+diff "$PEERTUBE_PATH/versions/peertube-${VERSION}/config/production.yaml.example" $PEERTUBE_PATH/config/production.yaml
 
