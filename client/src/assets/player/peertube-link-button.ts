fc73684adad4fdc6bb8a196bd54ef8ecec4e7391
Chocobozzz <me@florianbigard.com>
2018-05-28 11:35:18 +0200

    Resume video on peertube link click in embed

--- client/src/assets/player/peertube-link-button.ts
+++ client/src/assets/player/peertube-link-button.ts
@@ -4,17 +4,37 @@ const Button: VideoJSComponentInterface = videojsUntyped.getComponent('Button')
 class PeerTubeLinkButton extends Button {
 
   createEl () {
-    return videojsUntyped.dom.createEl('a', {
-      href: window.location.href.replace('embed', 'watch'),
+    return this.buildElement()
+  }
+
+  updateHref () {
+    const currentTime = Math.floor(this.player().currentTime())
+    this.el().setAttribute('href', this.buildHref(currentTime))
+  }
+
+  handleClick () {
+    this.player_.pause()
+  }
+
+  private buildElement () {
+    const el = videojsUntyped.dom.createEl('a', {
+      href: this.buildHref(),
       innerHTML: 'PeerTube',
       title: 'Go to the video page',
       className: 'vjs-peertube-link',
       target: '_blank'
     })
+
+    el.addEventListener('mouseenter', () => this.updateHref())
+
+    return el
   }
 
-  handleClick () {
-    this.player_.pause()
+  private buildHref (time?: number) {
+    let href = window.location.href.replace('embed', 'watch')
+    if (time) href += '?start=' + time
+
+    return href
   }
 }
 Button.registerComponent('PeerTubeLinkButton', PeerTubeLinkButton)
