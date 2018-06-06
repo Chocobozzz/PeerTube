96bb360d9f4ad410ad753cb0c4d8d0a7bfd033f4
Chocobozzz <me@florianbigard.com>
2018-06-06 18:17:24 +0200

    Update build with i18n

--- scripts/build/client.sh
+++ scripts/build/client.sh
@@ -6,14 +6,15 @@ cd client
 
 rm -rf ./dist ./compiled
 
-defaultLanguage="en-US"
+defaultLanguage="en_US"
 npm run ng build -- --output-path "dist/$defaultLanguage/" --deploy-url "/client/$defaultLanguage/" --prod --stats-json
 mv "./dist/$defaultLanguage/assets" "./dist"
 
-languages="fr"
+# Supported languages
+languages="fr_FR"
 
 for lang in "$languages"; do
-    npm run ng build -- --prod --i18n-file "./src/locale/target/messages_$lang.xml" --i18n-format xlf --i18n-locale "$lang" \
+    npm run ng build -- --prod --i18n-file "./src/locale/target/angular_$lang.xml" --i18n-format xlf --i18n-locale "$lang" \
         --output-path "dist/$lang/" --deploy-url "/client/$lang/"
 
     # Do no duplicate assets
@@ -22,3 +23,5 @@ done
 
 NODE_ENV=production npm run webpack -- --config webpack/webpack.video-embed.js --mode production
 
+# Copy runtime locales
+cp -r "./src/locale/target" "./dist/locale"
