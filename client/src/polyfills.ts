3ea9a1c311c3e3c55fb95560d4dd99a77c52df4a
William Lahti <wilahti@gmail.com>
2018-06-28 06:53:04 -0700

    Replace angular-cli patch with something less risky (#742)
    * Replace angular-cli patch with something less risky
    
    * path.join() is needed, provide a naive implementation
    
    * technically, webpack provided a polyfill for Buffer, we should too
    
    * process polyfill matches webpack; peertube depends on nextTick
    
    * polyfill for path to match webpack
    
    * http/https polyfills as per webpack

--- client/src/polyfills.ts
+++ client/src/polyfills.ts
@@ -80,3 +80,9 @@ import 'zone.js/dist/zone'  // Included with Angular CLI.
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
+
+// global/process polyfills
+
+;(window as any).global = window;
+;(window as any).process = require('process/');
+;(window as any).Buffer = require('buffer/').Buffer;
