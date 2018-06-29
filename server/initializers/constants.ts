8244c85ad668d38f984f975ddc3c7e4e93cde050
Chocobozzz <me@florianbigard.com>
2018-06-29 09:33:36 +0200

    Fix npm run dev:server

--- server/initializers/constants.ts
+++ server/initializers/constants.ts
@@ -452,7 +452,7 @@ const FEEDS = {
 
 const TRACKER_RATE_LIMITS = {
   INTERVAL: 60000 * 5, // 5 minutes
-  ANNOUNCES_PER_IP_PER_INFOHASH: 10, // maximum announces per torrent in the interval
+  ANNOUNCES_PER_IP_PER_INFOHASH: 15, // maximum announces per torrent in the interval
   ANNOUNCES_PER_IP: 30 // maximum announces for all our torrents in the interval
 }
 
