2db85d6b6ed5210e437d716abd4a068b69180165
Chocobozzz <me@florianbigard.com>
2018-08-27 16:34:41 +0200

    Upgrade server dependencies

--- scripts/watch/server.sh
+++ scripts/watch/server.sh
@@ -7,7 +7,7 @@ mkdir -p "./client/dist"
 rm -rf "./client/dist/locale"
 cp -r "./client/src/locale/target" "./client/dist/locale"
 
-rm -r "./dist"
+rm -rf "./dist"
 
 NODE_ENV=test npm run concurrently -- -k \
   "npm run tsc -- --sourceMap && npm run nodemon -- --delay 2 --watch ./dist dist/server" \
