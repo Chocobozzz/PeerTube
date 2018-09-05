8ff3f88347cb733ca8a3978ba6eb9763fb3ea294
Chocobozzz <me@florianbigard.com>
2018-09-05 11:20:44 +0200

    Fix watch page responsive

--- client/src/app/videos/+video-watch/video-watch.component.ts
+++ client/src/app/videos/+video-watch/video-watch.component.ts
@@ -317,10 +317,6 @@ export class VideoWatchComponent implements OnInit, OnDestroy {
     return this.video && this.video.state.id === VideoState.TO_IMPORT
   }
 
-  isMenuExpanded () {
-    return document.getElementById('right-container').classList.contains('expanded')
-  }
-
   hasVideoScheduledPublication () {
     return this.video && this.video.scheduledUpdate !== undefined
   }
