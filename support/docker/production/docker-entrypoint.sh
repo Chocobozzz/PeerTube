0356e301122c74defdbccff6273d42828df469a1
victor héry <git@lecygnenoir.info>
2018-09-22 21:03:42 +0200

    Docker: Add possibility to configure multiple trust_proxy

--- support/docker/production/docker-entrypoint.sh
+++ support/docker/production/docker-entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 set -e
 
 # Populate config directory
@@ -9,6 +9,18 @@ fi
 # Always copy default and custom env configuration file, in cases where new keys were added
 cp /app/config/default.yaml /config
 cp /app/support/docker/production/config/custom-environment-variables.yaml /config
+chown -R peertube:peertube /config
+
+# Parsing trust_proxy if available
+if [ ! -z "${PEERTUBE_TRUST_PROXY}" ]; then
+    # We need to replace space by comma
+    TRUST_PROXY="[${PEERTUBE_TRUST_PROXY// /,}]"
+    # Push the trust_proxy conf to production.yaml and remove old config
+    awk -v var="${TRUST_PROXY}" 's{if(/\s*-\s*.*/) next; else s=0}  /trust_proxy:/{print "trust_proxy: " var; s=1; next} 1' /config/production.yaml > /config/temp-production.yaml
+    # We use cat to override some filesystem problem with cp
+    cat /config/temp-production.yaml > /config/production.yaml
+    rm /config/temp-production.yaml
+fi
 chown -R peertube:peertube /config
 
 # first arg is `-f` or `--some-option`
