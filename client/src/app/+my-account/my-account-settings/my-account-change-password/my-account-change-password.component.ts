3805ce3f4335e89f63ddb6cd166f14ba120d9c83
BO41 <lukasw41@gmail.com>
2018-09-22 15:46:33 +0200

    check old password before change

--- client/src/app/+my-account/my-account-settings/my-account-change-password/my-account-change-password.component.ts
+++ client/src/app/+my-account/my-account-settings/my-account-change-password/my-account-change-password.component.ts
@@ -5,6 +5,8 @@ import { I18n } from '@ngx-translate/i18n-polyfill'
 import { FormValidatorService } from '@app/shared/forms/form-validators/form-validator.service'
 import { UserValidatorsService } from '@app/shared/forms/form-validators/user-validators.service'
 import { filter } from 'rxjs/operators'
+import { AuthService } from '@app/core';
+import { User } from '../../../../../../shared';
 
 @Component({
   selector: 'my-account-change-password',
@@ -13,11 +15,13 @@ import { filter } from 'rxjs/operators'
 })
 export class MyAccountChangePasswordComponent extends FormReactive implements OnInit {
   error: string = null
+  user: User = null
 
   constructor (
     protected formValidatorService: FormValidatorService,
     private userValidatorsService: UserValidatorsService,
     private notificationsService: NotificationsService,
+    private authService: AuthService,
     private userService: UserService,
     private i18n: I18n
   ) {
@@ -26,9 +30,12 @@ export class MyAccountChangePasswordComponent extends FormReactive implements On
 
   ngOnInit () {
     this.buildForm({
+      'old-password': this.userValidatorsService.USER_PASSWORD,
       'new-password': this.userValidatorsService.USER_PASSWORD,
       'new-confirmed-password': this.userValidatorsService.USER_CONFIRM_PASSWORD
     })
+
+    this.user = this.authService.getUser()
 
     const confirmPasswordControl = this.form.get('new-confirmed-password')
 
@@ -37,7 +44,23 @@ export class MyAccountChangePasswordComponent extends FormReactive implements On
                           .subscribe(() => confirmPasswordControl.setErrors({ matchPassword: true }))
   }
 
-  changePassword () {
+  checkPassword () {
+    this.error = null
+    const oldPassword = this.form.value[ 'old-password' ];
+
+    // compare old password
+    this.authService.login(this.user.account.name, oldPassword)
+      .subscribe(
+        () => this.changePassword(),
+        err => {
+          if (err.message.indexOf('credentials are invalid') !== -1) this.error = this.i18n('Incorrect old password.')
+          else this.error = err.message
+        }
+      )
+
+  }
+
+  private changePassword(){
     this.userService.changePassword(this.form.value[ 'new-password' ]).subscribe(
       () => {
         this.notificationsService.success(this.i18n('Success'), this.i18n('Password updated.'))
