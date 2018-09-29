527c86b79d57bebdb1b2d8c5c2f6cfe1d78a46a3
Felix Ableitner <me@nutomic.com>
2018-09-29 13:22:09 -0500

    move getUUIDFromFilename

--- scripts/optimize-old-videos.ts
+++ scripts/optimize-old-videos.ts
@@ -4,6 +4,8 @@ import { CONFIG } from '../server/initializers/constants'
 import { getVideoFileResolution, transcode, getVideoFileBitrate } from '../server/helpers/ffmpeg-utils'
 import { getMaxBitrate } from '../shared/models/videos'
 import { VideoRedundancyModel } from '../server/models/redundancy/video-redundancy'
+import { VideoModel } from '../server/models/video/video'
+import { getUUIDFromFilename } from '../server/helpers/utils'
 
 run()
   .then(() => process.exit(0))
@@ -22,7 +24,7 @@ async function run () {
 
     const isLocalVideo = await VideoRedundancyModel.isLocalByVideoUUIDExists(uuid)
     const isMaxBitrateExceeded = videoBitrate > getMaxBitrate(resolution.videoFileResolution)
-    if (uuid != null && isLocalVideo && isMaxBitrateExceeded) {
+    if (uuid && isLocalVideo && isMaxBitrateExceeded) {
       console.log(`Optimizing video ${ file }`)
       const outputPath = join(CONFIG.STORAGE.VIDEOS_DIR, uuid + '-optimized.mp4')
       await remove(outputPath)
@@ -35,17 +37,9 @@ async function run () {
 
       await transcode(transcodeOptions)
       await move(outputPath, inputPath, { overwrite: true })
+      const videoModel = VideoModel.loadByUUIDWithFile(uuid)
+      videoModel.createTorrentAndSetInfoHash()
     }
   }
   console.log('Finished optimizing videos')
-}
-
-// TODO: copied from prune-storage.ts, should be moved to some common file
-function getUUIDFromFilename (filename: string) {
-  const regex = /[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/
-  const result = filename.match(regex)
-
-  if (!result || Array.isArray(result) === false) return null
-
-  return result[0]
 }
