e1d7b98bc762bf3a5e6590f827f67187713fb27e
Rigel Kent <sendmemail@rigelk.eu>
2018-10-08 21:13:13 +0200

    fix automatic bitrate adjustment
    The regression was introduced in
    edb4ffc7e0b13659d7c73b120f2c87b27e4c26a1 and forced a fixed bitrate per
    resolution

--- server/helpers/ffmpeg-utils.ts
+++ server/helpers/ffmpeg-utils.ts
@@ -152,7 +152,7 @@ function transcode (options: TranscodeOptions) {
     // https://slhck.info/video/2017/03/01/rate-control.html
     // https://trac.ffmpeg.org/wiki/Limiting%20the%20output%20bitrate
     const targetBitrate = getTargetBitrate(options.resolution, fps, VIDEO_TRANSCODING_FPS)
-    command.outputOptions([`-b:v ${ targetBitrate }`, `-maxrate ${ targetBitrate }`, `-bufsize ${ targetBitrate * 2 }`])
+    command.outputOptions([`-maxrate ${ targetBitrate }`, `-bufsize ${ targetBitrate * 2 }`])
 
     command
       .on('error', (err, stdout, stderr) => {
