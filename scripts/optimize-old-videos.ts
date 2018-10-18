b36bf2828dde435b9b5f7a100fb30e5d65a5e2b5
Felix Ableitner <me@nutomic.com>
2018-10-18 15:06:37 -0500

    Make backups of files in optimize-old-videos.ts

--- scripts/optimize-old-videos.ts
+++ scripts/optimize-old-videos.ts
@@ -1,10 +1,11 @@
 import { CONFIG, VIDEO_TRANSCODING_FPS } from '../server/initializers/constants'
-import { getVideoFileBitrate, getVideoFileFPS, getVideoFileResolution } from '../server/helpers/ffmpeg-utils'
+import { getVideoFileBitrate, getVideoFileFPS, getVideoFileResolution, getDurationFromVideoFile } from '../server/helpers/ffmpeg-utils'
 import { getMaxBitrate } from '../shared/models/videos'
 import { VideoModel } from '../server/models/video/video'
 import { optimizeVideofile } from '../server/lib/video-transcoding'
 import { initDatabaseModels } from '../server/initializers'
-import { join } from 'path'
+import { join, basename } from 'path'
+import { copy, remove, move } from 'fs-extra'
 
 run()
   .then(() => process.exit(0))
@@ -28,9 +29,23 @@ async function run () {
         getVideoFileResolution(inputPath)
       ])
 
-      const isMaxBitrateExceeded = videoBitrate > getMaxBitrate(resolution.videoFileResolution, fps, VIDEO_TRANSCODING_FPS)
+      const maxBitrate = getMaxBitrate(resolution.videoFileResolution, fps, VIDEO_TRANSCODING_FPS)
+      const isMaxBitrateExceeded = videoBitrate > maxBitrate
       if (isMaxBitrateExceeded) {
+        console.log('Optimizing video file %s with bitrate %s kbps (max: %s kbps)',
+          basename(inputPath), videoBitrate / 1000, maxBitrate / 1000)
+        const backupFile = inputPath + '_backup'
+        await copy(inputPath, backupFile)
         await optimizeVideofile(video, file)
+        const originalDuration = await getDurationFromVideoFile(backupFile)
+        const newDuration = await getDurationFromVideoFile(inputPath)
+        if (originalDuration === newDuration) {
+          console.log('Finished optimizing %s', basename(inputPath))
+          remove(backupFile)
+        } else {
+          console.log('Failed to optimize %s, restoring original', inputPath)
+          move(backupFile, inputPath, { overwrite: true })
+        }
       }
     }
   }
