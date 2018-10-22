2489840d00b9b7be5f46b9768502dc74576cd534
victor héry <git@lecygnenoir.info>
2018-10-22 18:03:17 +0200

    Configure docker image to accept trust_proxy as an env variable in json array

--- support/docker/production/docker-entrypoint.sh
+++ support/docker/production/docker-entrypoint.sh
@@ -11,22 +11,6 @@ cp /app/config/default.yaml /config
 cp /app/support/docker/production/config/custom-environment-variables.yaml /config
 chown -R peertube:peertube /config
 
-# As trust_proxy is a yaml array in configuration and docker env doesn't allow array
-# we need to parse the env variable PEERTUBE_TRUST_PROXY ourselves
-# then write the array directly in the config file.
-# This part deals with PEERTUBE_TRUST_PROXY env variable only
-if [ ! -z "${PEERTUBE_TRUST_PROXY}" ]; then
-    # We need to replace space by comma
-    TRUST_PROXY="[${PEERTUBE_TRUST_PROXY// /,}]"
-    # Push the trust_proxy conf to production.yaml and remove old config
-    awk -v var="${TRUST_PROXY}" 's{if(/\s*-\s*.*/) next; else s=0}  /trust_proxy:/{print "trust_proxy: " var; s=1; next} 1' /config/production.yaml > /config/temp-production.yaml
-    # We use cat to override some filesystem problem with cp
-    cat /config/temp-production.yaml > /config/production.yaml
-    rm /config/temp-production.yaml
-fi
-# After modifying the config file, chown again to solve some security problems (k8s security context versus raw docker env)
-chown -R peertube:peertube /config
-
 # first arg is `-f` or `--some-option`
 # or first arg is `something.conf`
 if [ "${1#-}" != "$1" ] || [ "${1%.conf}" != "$1" ]; then
