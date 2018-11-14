b83b8dd5aef03084133c5983de6f312e7d1654b8
Rigel Kent <sendmemail@rigelk.eu>
2018-11-14 15:27:47 +0100

    add cli option to run without client

---
+++ server/tests/api/server/no-client.ts
@@ -0,0 +1,36 @@
+import 'mocha'
+import * as request from 'supertest'
+import {
+  flushTests,
+  killallServers,
+  ServerInfo
+} from '../../utils/index'
+import { runServer } from '../../utils/server/servers'
+
+describe('Start and stop server without web client routes', function () {
+  let server: ServerInfo
+
+  before(async function () {
+    this.timeout(30000)
+
+    await flushTests()
+
+    server = await runServer(1, {}, ['--no-client'])
+  })
+
+  it('Should fail getting the client', function () {
+    const req = request(server.url)
+      .get('/')
+
+    return req.expect(404)
+  })
+
+  after(async function () {
+    killallServers([ server ])
+
+    // Keep the logs if the test failed
+    if (this['ok']) {
+      await flushTests()
+    }
+  })
+})
