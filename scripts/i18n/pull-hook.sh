57b972698208ba5e393aac718995a60388360450
Chocobozzz <me@florianbigard.com>
2018-11-22 14:06:11 +0100

    Fix some zanata translations

--- scripts/i18n/pull-hook.sh
+++ scripts/i18n/pull-hook.sh
@@ -7,6 +7,7 @@ set -eu
 
 for i in 1 2 3; do
     perl -pi -e 's|&lt;x id=(.+?)/&gt;([^"])|<x id=\1/>\2|g' client/src/locale/target/*.xml
+    perl -0pi -e 's|<source>(.+?)</source>\s*<context-group |<source>\1</source><target>\1</target><context-group |g' client/src/locale/target/angular_*.xml
 done
 
 npm run i18n:xliff2json
