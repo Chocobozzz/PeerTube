d22b9b3e20c99ce2c079105673d2209aef64cec4
Chocobozzz <me@florianbigard.com>
2018-12-06 09:29:00 +0100

    Fix thumbnail sitemap location

--- server/controllers/bots.ts
+++ server/controllers/bots.ts
@@ -85,7 +85,7 @@ async function getSitemapLocalVideoUrls () {
         // Sitemap description should be < 2000 characters
         description: truncate(v.description || v.name, { length: 2000, omission: '...' }),
         player_loc: CONFIG.WEBSERVER.URL + '/videos/embed/' + v.uuid,
-        thumbnail_loc: v.getThumbnailStaticPath()
+        thumbnail_loc: CONFIG.WEBSERVER.URL + v.getThumbnailStaticPath()
       }
     ]
   }))
