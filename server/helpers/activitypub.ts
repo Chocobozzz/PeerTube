ae3171b66001bab3b67e6ea87190339e3cc3b677
Chocobozzz <me@florianbigard.com>
2019-04-08 09:24:04 +0200

    Fix jsonld context

--- server/helpers/activitypub.ts
+++ server/helpers/activitypub.ts
@@ -24,21 +24,54 @@ function activityPubContextify <T> (data: T) {
         subtitleLanguage: 'sc:subtitleLanguage',
         sensitive: 'as:sensitive',
         language: 'sc:inLanguage',
-        views: 'sc:Number',
-        state: 'sc:Number',
-        size: 'sc:Number',
-        fps: 'sc:Number',
-        startTimestamp: 'sc:Number',
-        stopTimestamp: 'sc:Number',
-        position: 'sc:Number',
-        commentsEnabled: 'sc:Boolean',
-        downloadEnabled: 'sc:Boolean',
-        waitTranscoding: 'sc:Boolean',
         expires: 'sc:expires',
-        support: 'sc:Text',
         CacheFile: 'pt:CacheFile',
         Infohash: 'pt:Infohash',
-        originallyPublishedAt: 'sc:DateTime'
+        originallyPublishedAt: 'sc:datePublished',
+        views: {
+          type: 'sc:Number',
+          '@id': 'pt:views'
+        },
+        state: {
+          type: 'sc:Number',
+          '@id': 'pt:state'
+        },
+        size: {
+          type: 'sc:Number',
+          '@id': 'pt:size'
+        },
+        fps: {
+          type: 'sc:Number',
+          '@id': 'pt:fps'
+        },
+        startTimestamp: {
+          type: 'sc:Number',
+          '@id': 'pt:startTimestamp'
+        },
+        stopTimestamp: {
+          type: 'sc:Number',
+          '@id': 'pt:stopTimestamp'
+        },
+        position: {
+          type: 'sc:Number',
+          '@id': 'pt:position'
+        },
+        commentsEnabled: {
+          type: 'sc:Boolean',
+          '@id': 'pt:commentsEnabled'
+        },
+        downloadEnabled: {
+          type: 'sc:Boolean',
+          '@id': 'pt:downloadEnabled'
+        },
+        waitTranscoding: {
+          type: 'sc:Boolean',
+          '@id': 'pt:waitTranscoding'
+        },
+        support: {
+          type: 'sc:Text',
+          '@id': 'pt:support'
+        }
       },
       {
         likes: {
