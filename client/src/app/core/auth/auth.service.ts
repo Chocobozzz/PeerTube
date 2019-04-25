c1521ca3d757bee91f7dfbb15b3717162bf4997d
Chocobozzz <me@florianbigard.com>
2019-04-25 10:03:30 +0200

    Case insensitive login

--- client/src/app/core/auth/auth.service.ts
+++ client/src/app/core/auth/auth.service.ts
@@ -153,7 +153,7 @@ export class AuthService {
       response_type: 'code',
       grant_type: 'password',
       scope: 'upload',
-      username,
+      username: username.toLowerCase(),
       password
     }
 
