f88392cf680c30943f91d79d96e65277dfb0c7f0
Chocobozzz <me@florianbigard.com>
2019-05-15 15:37:31 +0200

    Decrease max parallel tests for travis

--- server/tests/api/travis-4.sh
+++ server/tests/api/travis-4.sh
@@ -5,5 +5,5 @@ set -eu
 redundancyFiles=$(find server/tests/api/redundancy -type f | grep -v index.ts | xargs echo)
 activitypubFiles=$(find server/tests/api/activitypub -type f | grep -v index.ts | xargs echo)
 
-MOCHA_PARALLEL=true mocha-parallel-tests --max-parallel 3 --timeout 5000 --exit --require ts-node/register --bail \
+MOCHA_PARALLEL=true mocha-parallel-tests --max-parallel $1 --timeout 5000 --exit --require ts-node/register --bail \
     $redundancyFiles $activitypubFiles
