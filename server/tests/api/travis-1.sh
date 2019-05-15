d183ee9bda2588f07032ca4a2fce58651e684abf
Chocobozzz <me@florianbigard.com>
2019-05-15 17:17:14 +0200

    Don't use parallel tests with test suite 1 and 3

--- server/tests/api/travis-1.sh
+++ server/tests/api/travis-1.sh
@@ -6,5 +6,5 @@ checkParamFiles=$(find server/tests/api/check-params -type f | grep -v index.ts 
 notificationsFiles=$(find server/tests/api/notifications -type f | grep -v index.ts | xargs echo)
 searchFiles=$(find server/tests/api/search -type f | grep -v index.ts | xargs echo)
 
-MOCHA_PARALLEL=true mocha-parallel-tests --max-parallel $1 --timeout 5000 --exit --require ts-node/register --bail \
+MOCHA_PARALLEL=true mocha --timeout 5000 --exit --require ts-node/register --bail \
     $notificationsFiles $searchFiles $checkParamFiles
--- server/tests/api/travis-3.sh
+++ server/tests/api/travis-3.sh
@@ -4,5 +4,5 @@ set -eu
 
 videosFiles=$(find server/tests/api/videos -type f | grep -v index.ts | xargs echo)
 
-MOCHA_PARALLEL=true mocha-parallel-tests --max-parallel $1 --timeout 5000 --exit --require ts-node/register --bail \
+MOCHA_PARALLEL=true mocha --timeout 5000 --exit --require ts-node/register --bail \
     $videosFiles
