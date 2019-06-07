d4f87d83dde04529781bd3d70b1d05d9ed0198c1
Nassim Bounouas <nassim.bounouas@gmail.com>
2019-06-07 18:47:48 +0200

    #1843 Copy icon added to account page to copy Username

--- client/src/app/+accounts/accounts.component.ts
+++ client/src/app/+accounts/accounts.component.ts
@@ -70,4 +70,18 @@ export class AccountsComponent implements OnInit, OnDestroy {
           )
     }
   }
+
+  copyText(val: string){
+    let selBox = document.createElement('textarea');
+      selBox.style.position = 'fixed';
+      selBox.style.left = '0';
+      selBox.style.top = '0';
+      selBox.style.opacity = '0';
+      selBox.value = val;
+      document.body.appendChild(selBox);
+      selBox.focus();
+      selBox.select();
+      document.execCommand('copy');
+      document.body.removeChild(selBox);
+  }
 }
