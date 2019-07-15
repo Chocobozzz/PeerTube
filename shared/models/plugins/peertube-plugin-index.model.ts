7f1f23545422d5b533f429d448a6daaac2b2d830
Chocobozzz <me@florianbigard.com>
2019-07-15 15:41:56 +0200

    Add peertube plugin index website models

---
+++ shared/models/plugins/peertube-plugin-index.model.ts
@@ -0,0 +1,11 @@
+export interface PeerTubePluginIndex {
+  npmName: string
+  description: string
+  homepage: string
+  createdAt: Date
+  updatedAt: Date
+
+  popularity: number
+
+  latestVersion: string
+}
