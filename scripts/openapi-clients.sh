6916c5143c82551160dbf657a3d3fac5a526e061
LoveIsGrief <loveisgrief@tuta.io>
2019-08-19 19:54:03 +0200

    Pass more params to openapi generator and use vars

--- scripts/openapi-clients.sh
+++ scripts/openapi-clients.sh
@@ -4,12 +4,18 @@
 set -euo pipefail
 IFS=$'\n\t'
 
+OUT_DIR=dist/api/python
 
 docker run --rm -v ${PWD}:/local openapitools/openapi-generator-cli generate \
     -i /local/support/doc/api/openapi.yaml \
     -c /local/openapi/python.yaml
     -g python \
-    -o /local/dist/api/python
+    --git-user-id "${GIT_USER_ID}" \
+    --git-repo-id "${GIT_REPO_ID}" \
+    -o /local/$OUT_DIR
+
+# Docker uses root so we need to undo that
+sudo chown -R `id -u` $OUT_DIR
 
 # Will use #$GIT_USER $GIT_REPO_ID and $GIT_TOKEN to update repo upon build
 bash $OUT_DIR/git_push.sh "${GIT_USER_ID}" "${GIT_REPO_ID}"
