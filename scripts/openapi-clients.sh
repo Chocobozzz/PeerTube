f0d56a83c678d8a6f41ebfde7dac66dd1f4d524f
LoveIsGrief <loveisgrief@tuta.io>
2019-08-19 18:25:45 +0200

    Update the targeted directory for the openapi command

--- scripts/openapi-clients.sh
+++ scripts/openapi-clients.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 docker run --rm -v ${PWD}:/local openapitools/openapi-generator-cli generate \
-    -i support/doc/api/openapi.yaml
+    -i /local/support/doc/api/openapi.yaml
     -l python \
     -o /local/out/python
