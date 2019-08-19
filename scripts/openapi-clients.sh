51051ab584b2a74e993ec38c29cc37020dab80a4
LoveIsGrief <loveisgrief@tuta.io>
2019-08-19 19:14:02 +0200

    Push successful builds to a new repo

--- scripts/openapi-clients.sh
+++ scripts/openapi-clients.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
+# Unofficial bash strict mode
+# https://web.archive.org/web/20190115051613/https://redsymbol.net/articles/unofficial-bash-strict-mode/
+set -euo pipefail
+IFS=$'\n\t'
+
 
 docker run --rm -v ${PWD}:/local openapitools/openapi-generator-cli generate \
     -i /local/support/doc/api/openapi.yaml \
     -g python \
     -o /local/dist/api/python
+
+# Will use #$GIT_USER $GIT_REPO_ID and $GIT_TOKEN to update repo upon build
+dist/api/python/git_push.sh
