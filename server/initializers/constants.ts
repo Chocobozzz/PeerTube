85c1df6ae899be2b1bdd9691b38a2a2b94bffdcf
Chocobozzz <me@florianbigard.com>
2019-09-24 10:35:36 +0200

    Add auto play next video migration

--- server/initializers/constants.ts
+++ server/initializers/constants.ts
@@ -14,7 +14,7 @@ import { CONFIG, registerConfigChangedHandler } from './config'
 
 // ---------------------------------------------------------------------------
 
-const LAST_MIGRATION_VERSION = 435
+const LAST_MIGRATION_VERSION = 440
 
 // ---------------------------------------------------------------------------
 
