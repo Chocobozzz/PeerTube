4c8ef0ec684f5dd1856ec751420b1e85369fd1bf
Chocobozzz <me@florianbigard.com>
2019-10-21 16:24:06 +0200

    Add tsconfig-paths registration to mocha

--- server/tests/api/ci-3.sh
+++ server/tests/api/ci-3.sh
@@ -4,5 +4,5 @@ set -eu
 
 videosFiles=$(find server/tests/api/videos -type f | grep -v index.ts | xargs echo)
 
-npm run mocha -- --timeout 30000 --exit --require ts-node/register --bail \
+npm run mocha -- --timeout 30000 --exit --require ts-node/register --require tsconfig-paths/register --bail \
     $videosFiles
