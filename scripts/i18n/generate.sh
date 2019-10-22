1cd4fe501de78e67cfdd19ea44989393a29ec2e6
Chocobozzz <me@florianbigard.com>
2019-10-22 10:30:09 +0200

    Fix i18n generation script

--- scripts/i18n/generate.sh
+++ scripts/i18n/generate.sh
@@ -3,7 +3,7 @@
 set -eu
 
 cd client
-npm run ng -- xi18n --i18n-locale "en-US" --output-path locale/source --out-file angular_en_US.xml
+npm run ng -- xi18n --i18n-locale "en-US" --output-path src/locale/source --out-file angular_en_US.xml
 npm run ngx-extractor -- --locale "en-US" -i 'src/**/*.ts' -f xlf -o src/locale/source/angular_en_US.xml
 
 # Zanata does not support inner elements in <source>, so we hack these special elements
