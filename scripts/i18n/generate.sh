3aea8eb2e7ee8fa53e05298030eaeef98d234797
Chocobozzz <me@florianbigard.com>
2019-11-07 09:45:14 +0100

    Try new worflow with french

--- scripts/i18n/generate.sh
+++ scripts/i18n/generate.sh
@@ -3,12 +3,11 @@
 set -eu
 
 cd client
-npm run ng -- xi18n --i18n-locale "en-US" --output-path src/locale/source --out-file angular_en_US.xml
-npm run ngx-extractor -- --locale "en-US" -i 'src/**/*.ts' -f xlf -o src/locale/source/angular_en_US.xml
+npm run ng -- xi18n --i18n-locale "en-US" --output-path src/locale --out-file angular.xlf
+npm run ngx-extractor -- --locale "en-US" -i 'src/**/*.ts' -f xlf -o src/locale/angular.xlf
 
-# Zanata does not support inner elements in <source>, so we hack these special elements
-# This regex translate the Angular elements to special entities (that we will reconvert on pull)
-perl -pi -e 's|<x id=(.+?)/>|&lt;x id=\1/&gt;|g' src/locale/source/angular_en_US.xml
+# Merge new translations in other language files
+npm run ng run -- PeerTube:xliffmerge
 
 # Add our strings too
 cd ../
