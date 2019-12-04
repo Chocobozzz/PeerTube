44df5c755c31798e64eba1ec41dd7e2d7ef50e56
Chocobozzz <me@florianbigard.com>
2019-12-04 13:29:40 +0100

    Skip channel search on empty search

--- client/src/app/search/search.component.ts
+++ client/src/app/search/search.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnDestroy, OnInit } from '@angular/core'
 import { ActivatedRoute, Router } from '@angular/router'
 import { AuthService, Notifier } from '@app/core'
-import { forkJoin, Subscription } from 'rxjs'
+import { forkJoin, of, Subscription } from 'rxjs'
 import { SearchService } from '@app/search/search.service'
 import { ComponentPagination } from '@app/shared/rest/component-pagination.model'
 import { I18n } from '@ngx-translate/i18n-polyfill'
@@ -184,6 +184,8 @@ export class SearchComponent implements OnInit, OnDestroy {
   }
 
   private getVideoChannelObs () {
+    if (!this.currentSearch) return of({ data: [], total: 0 })
+
     const params = {
       search: this.currentSearch,
       componentPagination: immutableAssign(this.pagination, { itemsPerPage: this.channelsPerPage })
