d816f3a063febac1cad09ab3a32e5f0d29353627
Rigel Kent <sendmemail@rigelk.eu>
2019-12-10 23:15:09 +0100

    autoplay next video switch for both user and visitors

--- client/src/app/videos/recommendations/recommended-videos.component.ts
+++ client/src/app/videos/recommendations/recommended-videos.component.ts
@@ -4,12 +4,18 @@ import { Video } from '@app/shared/video/video.model'
 import { RecommendationInfo } from '@app/shared/video/recommendation-info.model'
 import { RecommendedVideosStore } from '@app/videos/recommendations/recommended-videos.store'
 import { User } from '@app/shared'
+import { AuthService, Notifier } from '@app/core'
+import { UserService } from '@app/shared/users/user.service'
+import { peertubeLocalStorage } from '@app/shared/misc/peertube-local-storage'
 
 @Component({
   selector: 'my-recommended-videos',
-  templateUrl: './recommended-videos.component.html'
+  templateUrl: './recommended-videos.component.html',
+  styleUrls: [ './recommended-videos.component.scss' ]
 })
 export class RecommendedVideosComponent implements OnChanges {
+  private static LOCAL_STORAGE_AUTO_PLAY_NEXT_VIDEO = 'auto_play_next_video'
+
   @Input() inputRecommendation: RecommendationInfo
   @Input() user: User
   @Output() gotRecommendations = new EventEmitter<Video[]>()
@@ -17,12 +23,21 @@ export class RecommendedVideosComponent implements OnChanges {
   readonly hasVideos$: Observable<boolean>
   readonly videos$: Observable<Video[]>
 
+  private autoPlayNextVideo: boolean
+
   constructor (
+    private userService: UserService,
+    private authService: AuthService,
+    private notifier: Notifier,
     private store: RecommendedVideosStore
   ) {
     this.videos$ = this.store.recommendations$
     this.hasVideos$ = this.store.hasRecommendations$
     this.videos$.subscribe(videos => this.gotRecommendations.emit(videos))
+
+    this.autoPlayNextVideo = this.authService.isLoggedIn()
+      ? this.authService.getUser().autoPlayNextVideo
+      : peertubeLocalStorage.getItem(RecommendedVideosComponent.LOCAL_STORAGE_AUTO_PLAY_NEXT_VIDEO) === 'true' || false
   }
 
   public ngOnChanges (): void {
@@ -33,5 +48,22 @@ export class RecommendedVideosComponent implements OnChanges {
 
   onVideoRemoved () {
     this.store.requestNewRecommendations(this.inputRecommendation)
+  }
+
+  switchAutoPlayNextVideo () {
+    peertubeLocalStorage.setItem(RecommendedVideosComponent.LOCAL_STORAGE_AUTO_PLAY_NEXT_VIDEO, this.autoPlayNextVideo.toString())
+
+    if (this.authService.isLoggedIn()) {
+      const details = {
+        autoPlayNextVideo: this.autoPlayNextVideo
+      }
+
+      this.userService.updateMyProfile(details).subscribe(
+        () => {
+          this.authService.refreshUserInformation()
+        },
+        err => this.notifier.error(err.message)
+      )
+    }
   }
 }
