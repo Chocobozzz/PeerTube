e43ed96ef494a9f0128dcfc48af3eb23c6186e91
Chocobozzz <me@florianbigard.com>
2019-12-18 15:40:42 +0100

    Fix build script

--- scripts/build/index.sh
+++ scripts/build/index.sh
@@ -2,12 +2,12 @@
 
 set -eu
 
-if [[ -n ${1+x} ]]; then
+if [ ! -z ${1+x} ]; then
   clientCommand="npm run build:client -- $1"
 else
   clientCommand="npm run build:client"
 fi
 
-npm run concurrently -- --raw \w
+npm run concurrently -- --raw \
   "$clientCommand" \
   "npm run build:server"
