9b7668f527fce8981f4684ed0459b62e740b4b82
Chocobozzz <me@florianbigard.com>
2020-01-07 16:53:55 +0100

    Fix npm run dev script

--- scripts/dev/client.sh
+++ scripts/dev/client.sh
@@ -2,6 +2,15 @@
 
 set -eu
 
-NODE_ENV=test npm run concurrently -- -k \
-  "cd client && npm run ng -- serve --proxy-config proxy.config.json --hmr --configuration hmr --host 0.0.0.0 --disable-host-check --port 3000" \
-  "npm run build:server && NODE_ENV=test npm start"
+clientCommand="cd client && npm run ng -- serve --proxy-config proxy.config.json --hmr --configuration hmr --host 0.0.0.0 --disable-host-check --port 3000"
+serverCommand="npm run build:server && NODE_ENV=test npm start"
+
+if [ ! -z ${1+x} ] && [ "$1" == "--skip-server" ]; then
+  NODE_ENV=test eval $clientCommand
+else
+  NODE_ENV=test npm run concurrently -- -k \
+    "$clientCommand" \
+    "$serverCommand"
+fi
+
+
--- scripts/dev/index.sh
+++ scripts/dev/index.sh
@@ -3,5 +3,5 @@
 set -eu
 
 NODE_ENV=test npm run concurrently -- -k \
-  "npm run dev:client" \
+  "npm run dev:client -- --skip-server" \
   "npm run dev:server"
