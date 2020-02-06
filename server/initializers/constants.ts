cef4e38f1c9f62c83caa26525a94742ee086f6f1
Chocobozzz <me@florianbigard.com>
2020-02-06 17:07:29 +0100

    Remove octet stream fallback for video extension
    It could be a FLAC file for example

--- server/initializers/constants.ts
+++ server/initializers/constants.ts
@@ -795,7 +795,6 @@ function buildVideoMimetypeExt () {
         'video/x-msvideo': '.avi',
         'video/x-flv': '.flv',
         'video/x-matroska': '.mkv',
-        'application/octet-stream': '.mkv',
         'video/avi': '.avi',
         'video/x-m4v': '.m4v',
         'video/m4v': '.m4v'
