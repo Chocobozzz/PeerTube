5ecda213b7c88e8e48da8199f8fecd87bfae8a8e
Chocobozzz <me@florianbigard.com>
2020-02-07 10:21:31 +0100

    Add @angular/localize

--- client/src/polyfills.ts
+++ client/src/polyfills.ts
@@ -69,3 +69,5 @@ import 'zone.js/dist/zone'  // Included with Angular CLI.
 ;(window as any).global = window
 ;(window as any).process = require('process/')
 ;(window as any).Buffer = require('buffer/').Buffer
+
+import '@angular/localize/init'
