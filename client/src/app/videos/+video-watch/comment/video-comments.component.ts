Chocobozzz/PeerTube

commit 0840ca76a2de3a2907aa64c2cdaf3d2c69b691c2

Add action hook, comments loaded (#2539)
* Adding action hook for video watch comments loaded

* Adding requested changes for PR #2539

client/src/app/videos/+video-watch/comment/video-comments.component.ts
@@ -130,6 +130,7 @@ export class VideoCommentsComponent implements OnInit, OnChanges, OnDestroy {
         this.componentPagination.totalItems = res.total
 
         this.onDataSubject.next(res.data)
+        this.hooks.runAction('action:video-watch.video-threads.loaded', 'video-watch', { data: this.componentPagination })
       },
 
       err => this.notifier.error(err.message)

shared/models/plugins/client-hook.model.ts
@@ -65,6 +65,8 @@ export const clientActionHookObject = {
   'action:video-watch.video.loaded': true,
   // Fired when the player finished loading
   'action:video-watch.player.loaded': true,
+  // Fired when the video watch page comments(threads) are loaded and load more comments on scroll
+  'action:video-watch.video-threads.loaded': true,
 
   // Fired when the search page is being initialized
   'action:search.init': true,

commit 0503e975b1ae6a85ddd2b8b89af8b412f848c321

Fix lint error introduced in #2539

client/src/app/videos/+video-watch/comment/video-comments.component.ts
@@ -193,7 +193,7 @@ export class VideoCommentsComponent implements OnInit, OnChanges, OnDestroy {
     return this.authService.isLoggedIn()
   }
 
-  onNearOfBottom () {    
+  onNearOfBottom () {
     if (hasMoreItems(this.componentPagination)) {
       this.componentPagination.currentPage++
       this.loadMoreThreads()