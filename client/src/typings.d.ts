18eb77a3b6392ed9927895ff291439ea11acc1ee
kimsible <kimsible@users.noreply.github.com>
2020-04-21 12:20:02 +0200

    Add ResizeObserver typing definition

--- client/src/typings.d.ts
+++ client/src/typings.d.ts
@@ -4,3 +4,10 @@ declare var module: NodeModule
 interface NodeModule {
   id: string
 }
+
+/* ResizeObserver API definition */
+declare global { /* tslint:disable */
+  interface Window { ResizeObserver: any }
+}
+
+window.ResizeObserver = window.ResizeObserver || {} /* tslint:disable */
