ecafc17f10541495718d7d881f53fbcc12fe41be
kimsible <kimsible@users.noreply.github.com>
2020-04-24 03:16:24 +0200

    Use modal instead of dropdown menu in small/mobile views

--- client/src/app/shared/menu/top-menu-dropdown.component.ts
+++ client/src/app/shared/menu/top-menu-dropdown.component.ts
@@ -1,8 +1,17 @@
-import { Component, Input, OnDestroy, OnInit } from '@angular/core'
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  HostListener,
+  Input,
+  OnDestroy,
+  OnInit,
+  ViewChild
+} from '@angular/core'
 import { filter, take } from 'rxjs/operators'
 import { NavigationEnd, Router } from '@angular/router'
 import { Subscription } from 'rxjs'
-import { NgbDropdown } from '@ng-bootstrap/ng-bootstrap'
+import { NgbDropdown, NgbModal } from '@ng-bootstrap/ng-bootstrap'
 import { GlobalIconName } from '@app/shared/images/global-icon.component'
 import { ScreenService } from '@app/shared/misc/screen.service'
 
@@ -23,18 +32,24 @@ export type TopMenuDropdownParam = {
   templateUrl: './top-menu-dropdown.component.html',
   styleUrls: [ './top-menu-dropdown.component.scss' ]
 })
-export class TopMenuDropdownComponent implements OnInit, OnDestroy {
+export class TopMenuDropdownComponent implements OnInit, OnDestroy, AfterViewInit {
   @Input() menuEntries: TopMenuDropdownParam[] = []
+
+  @ViewChild('modal', { static: true }) modal: ElementRef
 
   suffixLabels: { [ parentLabel: string ]: string }
   hasIcons = false
   container: undefined | 'body' = undefined
+  isInSmallView = false
+  isModalOpened = false
+  currentMenuEntryIndex: number
 
   private openedOnHover = false
   private routeSub: Subscription
 
   constructor (
     private router: Router,
+    private modalService: NgbModal,
     private screen: ScreenService
   ) {}
 
@@ -42,17 +57,22 @@ export class TopMenuDropdownComponent implements OnInit, OnDestroy {
     this.updateChildLabels(window.location.pathname)
 
     this.routeSub = this.router.events
-                        .pipe(filter(event => event instanceof NavigationEnd))
-                        .subscribe(() => this.updateChildLabels(window.location.pathname))
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe(() => this.updateChildLabels(window.location.pathname))
 
     this.hasIcons = this.menuEntries.some(
       e => e.children && e.children.some(c => !!c.iconName)
     )
 
-    // We have to set body for the container to avoid scroll overflow on mobile view
-    if (this.screen.isInMobileView()) {
+    // We have to set body for the container to avoid scroll overflow on mobile and small views
+    if (this.screen.isInSmallView()) {
       this.container = 'body'
+      this.isInSmallView = true
     }
+  }
+
+  ngAfterViewInit () {
+    setTimeout(() => this.onWindowResize(), 0)
   }
 
   ngOnDestroy () {
@@ -83,6 +103,31 @@ export class TopMenuDropdownComponent implements OnInit, OnDestroy {
 
     dropdown.close()
     this.openedOnHover = false
+  }
+
+  @HostListener('window:resize')
+  onWindowResize () {
+    this.isInSmallView = !!this.screen.isInSmallView()
+  }
+
+  openModal (index: number) {
+    this.currentMenuEntryIndex = index
+    this.isModalOpened = true
+
+    this.modalService.open(this.modal, {
+      centered: true, beforeDismiss: async () => {
+        this.onModalDismiss()
+        return true
+      }
+    })
+  }
+
+  onModalDismiss () {
+    this.isModalOpened = false
+  }
+
+  dismissOtherModals () {
+    this.modalService.dismissAll()
   }
 
   private updateChildLabels (path: string) {
