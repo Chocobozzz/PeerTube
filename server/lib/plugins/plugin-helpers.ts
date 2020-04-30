8d8c61172da612b0a471154eaf80af4d4fa215f8
Chocobozzz <me@florianbigard.com>
2020-04-30 08:46:40 +0200

    Add config helper for plugins

--- server/lib/plugins/plugin-helpers.ts
+++ server/lib/plugins/plugin-helpers.ts
@@ -2,6 +2,7 @@ import { PeerTubeHelpers } from '@server/typings/plugins'
 import { sequelizeTypescript } from '@server/initializers/database'
 import { buildLogger } from '@server/helpers/logger'
 import { VideoModel } from '@server/models/video/video'
+import { WEBSERVER } from '@server/initializers/constants'
 
 function buildPluginHelpers (npmName: string): PeerTubeHelpers {
   const logger = buildPluginLogger(npmName)
@@ -9,10 +10,13 @@ function buildPluginHelpers (npmName: string): PeerTubeHelpers {
   const database = buildDatabaseHelpers()
   const videos = buildVideosHelpers()
 
+  const config = buildConfigHelpers()
+
   return {
     logger,
     database,
-    videos
+    videos,
+    config
   }
 }
 
@@ -40,6 +44,14 @@ function buildVideosHelpers () {
 
         await video.destroy({ transaction: t })
       })
+    }
+  }
+}
+
+function buildConfigHelpers () {
+  return {
+    getWebserverUrl () {
+      return WEBSERVER.URL
     }
   }
 }
