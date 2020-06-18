07aea1a2642fc9868cb01e30c322514029d5b95a
Chocobozzz <me@florianbigard.com>
2020-06-18 17:12:24 +0200

    Use TS_NODE_FILES instead of --files
    Because --files does not work

--- server/tests/api/ci-4.sh
+++ server/tests/api/ci-4.sh
@@ -5,6 +5,6 @@ set -eu
 redundancyFiles=$(find server/tests/api/redundancy -type f | grep -v index.ts | xargs echo)
 activitypubFiles=$(find server/tests/api/activitypub -type f | grep -v index.ts | xargs echo)
 
-MOCHA_PARALLEL=true npm run mocha -- --parallel --jobs $1 --timeout 30000 --exit \
-  --require ts-node/register --files --require tsconfig-paths/register --bail \
+TS_NODE_FILES=true MOCHA_PARALLEL=true npm run mocha -- --parallel --jobs $1 --timeout 30000 --exit \
+  --require ts-node/register --require tsconfig-paths/register --bail \
   $redundancyFiles $activitypubFiles
