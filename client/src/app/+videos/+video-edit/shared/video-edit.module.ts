187b2e23465688acbefad52721593a51caa21c40
Chocobozzz <me@florianbigard.com>
2020-06-24 11:43:16 +0200

    Fix upload page

--- client/src/app/+videos/+video-edit/shared/video-edit.module.ts
+++ client/src/app/+videos/+video-edit/shared/video-edit.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core'
 import { SharedFormModule } from '@app/shared/shared-forms'
 import { SharedGlobalIconModule } from '@app/shared/shared-icons'
 import { SharedMainModule } from '@app/shared/shared-main'
+import { I18nPrimengCalendarService } from './i18n-primeng-calendar.service'
 import { VideoCaptionAddModalComponent } from './video-caption-add-modal.component'
 import { VideoEditComponent } from './video-edit.component'
 
@@ -33,6 +34,8 @@ import { VideoEditComponent } from './video-edit.component'
     VideoEditComponent
   ],
 
-  providers: []
+  providers: [
+    I18nPrimengCalendarService
+  ]
 })
 export class VideoEditModule { }
