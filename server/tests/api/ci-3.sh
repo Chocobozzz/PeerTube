7abb5c5da1ed7c6945e81b9e3da0a14f623f69ea
Chocobozzz <me@florianbigard.com>
2020-07-30 14:44:08 +0200

    Use parallel to run tests in parallel

--- server/tests/api/ci-3.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-
-set -eu
-
-videosFiles=$(find server/tests/api/videos -type f | grep -v index.ts | xargs echo)
-
-npm run mocha -- --timeout 30000 --exit --require ts-node/register --require tsconfig-paths/register --bail \
-    $videosFiles
