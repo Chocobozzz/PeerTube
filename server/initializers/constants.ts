1179a97e04ff5a2dc728f1f563e5f0a95e0ac39c
Chocobozzz <me@florianbigard.com>
2020-08-18 11:09:00 +0200

    Add server migration for playlists

--- server/initializers/constants.ts
+++ server/initializers/constants.ts
@@ -23,7 +23,7 @@ import { CONFIG, registerConfigChangedHandler } from './config'
 
 // ---------------------------------------------------------------------------
 
-const LAST_MIGRATION_VERSION = 525
+const LAST_MIGRATION_VERSION = 530
 
 // ---------------------------------------------------------------------------
 
