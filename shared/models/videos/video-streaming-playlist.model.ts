b2c76204f9d2900b9fd58e6618e343059821d601
Chocobozzz <me@florianbigard.com>
2020-08-27 10:08:54 +0200

    Fix VideoStreamingPlaylist model

--- shared/models/videos/video-streaming-playlist.model.ts
+++ shared/models/videos/video-streaming-playlist.model.ts
@@ -1,7 +1,7 @@
 import { VideoStreamingPlaylistType } from './video-streaming-playlist.type'
 import { VideoFile } from './video-file.model'
 
-export class VideoStreamingPlaylist {
+export interface VideoStreamingPlaylist {
   id: number
   type: VideoStreamingPlaylistType
   playlistUrl: string
