52a3f561f5357d076ddc36897d5e1d0fc60a8730
Rigel Kent <sendmemail@rigelk.eu>
2021-01-17 18:13:05 +0100

    add username information in profile settings

--- client/src/app/+my-account/my-account-settings/my-account-profile/my-account-profile.component.ts
+++ client/src/app/+my-account/my-account-settings/my-account-profile/my-account-profile.component.ts
@@ -25,16 +25,23 @@ export class MyAccountProfileComponent extends FormReactive implements OnInit {
 
   ngOnInit () {
     this.buildForm({
+      'user-name': null,
       'display-name': USER_DISPLAY_NAME_REQUIRED_VALIDATOR,
       description: USER_DESCRIPTION_VALIDATOR
     })
+    this.form.controls['user-name'].disable()
 
     this.userInformationLoaded.subscribe(() => {
       this.form.patchValue({
+        'user-name': this.user.username,
         'display-name': this.user.account.displayName,
         description: this.user.account.description
       })
     })
+  }
+
+  get instanceHost () {
+    return window.location.host
   }
 
   updateMyProfile () {
