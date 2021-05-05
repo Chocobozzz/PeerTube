a0e69167ae43ffbe088b2e9e436bf2624a4a1b4e
Chocobozzz <me@florianbigard.com>
2021-05-05 10:50:20 +0200

    Remove scripty
    It breaks our tests with the latest npm release

--- scripts/danger/clean/modules.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -eu
-
-read -p "This will remove all node server and client modules. Are you sure? " -n 1 -r
-
-if [[ "$REPLY" =~ ^[Yy]$ ]]; then
-  rm -rf node_modules client/node_modules
-fi
