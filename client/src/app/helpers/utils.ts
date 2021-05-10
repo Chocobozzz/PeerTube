1fa23d6f5e487f3c149e1f0001beadd919ee82fc
Chocobozzz <me@florianbigard.com>
2021-05-10 15:48:10 +0200

    Fix video update

--- client/src/app/helpers/utils.ts
+++ client/src/app/helpers/utils.ts
@@ -1,4 +1,4 @@
-import { map } from 'rxjs/operators'
+import { first, map } from 'rxjs/operators'
 import { SelectChannelItem } from 'src/types/select-options-item.model'
 import { DatePipe } from '@angular/common'
 import { HttpErrorResponse } from '@angular/common/http'
@@ -23,26 +23,29 @@ function getParameterByName (name: string, url: string) {
 
 function listUserChannels (authService: AuthService) {
   return authService.userInformationLoaded
-    .pipe(map(() => {
-      const user = authService.getUser()
-      if (!user) return undefined
+    .pipe(
+      first(),
+      map(() => {
+        const user = authService.getUser()
+        if (!user) return undefined
 
-      const videoChannels = user.videoChannels
-      if (Array.isArray(videoChannels) === false) return undefined
+        const videoChannels = user.videoChannels
+        if (Array.isArray(videoChannels) === false) return undefined
 
-      return videoChannels
-        .sort((a, b) => {
-          if (a.updatedAt < b.updatedAt) return 1
-          if (a.updatedAt > b.updatedAt) return -1
-          return 0
-        })
-        .map(c => ({
-          id: c.id,
-          label: c.displayName,
-          support: c.support,
-          avatarPath: c.avatar?.path
-        }) as SelectChannelItem)
-    }))
+        return videoChannels
+          .sort((a, b) => {
+            if (a.updatedAt < b.updatedAt) return 1
+            if (a.updatedAt > b.updatedAt) return -1
+            return 0
+          })
+          .map(c => ({
+            id: c.id,
+            label: c.displayName,
+            support: c.support,
+            avatarPath: c.avatar?.path
+          }) as SelectChannelItem)
+      })
+    )
 }
 
 function getAbsoluteAPIUrl () {
