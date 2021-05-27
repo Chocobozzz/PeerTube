8e5c2fdc335107533e7cad0fb724e4a0283249c3
Chocobozzz <me@florianbigard.com>
2021-05-27 16:31:54 +0200

    Fix home routing

--- client/src/app/+home/home-routing.module.ts
+++ client/src/app/+home/home-routing.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
-import { MetaGuard } from '@ngx-meta/core'
 import { HomeComponent } from './home.component'
 
 const homeRoutes: Routes = [
   {
     path: '',
-    component: HomeComponent,
-    canActivateChild: [ MetaGuard ]
+    component: HomeComponent
   }
 ]
 
