75e12406e271e3aaf1f7c394a63ce570091db480
Chocobozzz <me@florianbigard.com>
2021-06-08 16:52:35 +0200

    Increase tests timeout for plugin helpers

--- server/tests/plugins/plugin-helpers.ts
+++ server/tests/plugins/plugin-helpers.ts
@@ -144,7 +144,7 @@ describe('Test plugin helpers', function () {
     let videoUUIDServer1: string
 
     before(async function () {
-      this.timeout(30000)
+      this.timeout(60000)
 
       {
         const res = await uploadVideoAndGetId({ server: servers[0], videoName: 'video server 1' })
