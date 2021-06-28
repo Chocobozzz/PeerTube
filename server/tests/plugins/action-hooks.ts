9a66a4bc743daa7d9d6bceb9a51f47e0f510e9fc
Chocobozzz <me@florianbigard.com>
2021-06-28 09:21:49 +0200

    Fix tests

--- server/tests/plugins/action-hooks.ts
+++ server/tests/plugins/action-hooks.ts
@@ -70,37 +70,8 @@ describe('Test plugin action hooks', function () {
     })
   })
 
-  describe('Playlist hooks', function () {
-    let playlistId
-    let videoId
-
-    before(async function () {
-      const { body } = await createVideoPlaylist({
-        url: servers[0].url,
-        token: servers[0].accessToken,
-        playlistAttrs: {
-          displayName: 'My playlist',
-          privacy: VideoPlaylistPrivacy.PUBLIC
-        }
-      })
-      playlistId = body.videoPlaylist.id
-
-      const res = await uploadVideo(servers[0].url, servers[0].accessToken, { name: 'my super name' })
-      videoId = res.body.video.id
-    })
-
-    it('Should run action:api.video-playlist-element.created', async function () {
-      await addVideoInPlaylist({
-        url: servers[0].url,
-        token: servers[0].accessToken,
-        playlistId,
-        elementAttrs: { videoId }
-      })
-      await checkHook('action:api.video-playlist-element.created')
-    })
-  })
-
   describe('Videos hooks', function () {
+
     it('Should run action:api.video.uploaded', async function () {
       const res = await uploadVideo(servers[0].url, servers[0].accessToken, { name: 'video' })
       videoUUID = res.body.video.uuid
@@ -206,6 +177,41 @@ describe('Test plugin action hooks', function () {
       await removeUser(servers[0].url, userId, servers[0].accessToken)
 
       await checkHook('action:api.user.deleted')
+    })
+  })
+
+  describe('Playlist hooks', function () {
+    let playlistId: number
+    let videoId: number
+
+    before(async function () {
+      {
+        const res = await createVideoPlaylist({
+          url: servers[0].url,
+          token: servers[0].accessToken,
+          playlistAttrs: {
+            displayName: 'My playlist',
+            privacy: VideoPlaylistPrivacy.PRIVATE
+          }
+        })
+        playlistId = res.body.videoPlaylist.id
+      }
+
+      {
+        const res = await uploadVideo(servers[0].url, servers[0].accessToken, { name: 'my super name' })
+        videoId = res.body.video.id
+      }
+    })
+
+    it('Should run action:api.video-playlist-element.created', async function () {
+      await addVideoInPlaylist({
+        url: servers[0].url,
+        token: servers[0].accessToken,
+        playlistId,
+        elementAttrs: { videoId }
+      })
+
+      await checkHook('action:api.video-playlist-element.created')
     })
   })
 
