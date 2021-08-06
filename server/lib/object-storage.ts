25d6608d34681d034357944a08743fcbd5951964
Jelle Besseling <jelle@pingiun.com>
2021-08-06 14:05:12 +0200

    Use dynamic part size for really large uploads
    Also adds very small part size for local testing

--- server/lib/object-storage.ts
+++ server/lib/object-storage.ts
@@ -14,17 +14,26 @@ import { CONFIG } from "@server/initializers/config"
 import { logger } from '@server/helpers/logger'
 import { createReadStream, createWriteStream, ensureDir, open, close, ReadStream, stat, Stats } from "fs-extra"
 import { Readable } from "stream"
-import { pipeline } from "stream/promises"
 import { dirname } from "path"
 import { min } from "lodash"
 
 type BucketInfo = {BUCKET_NAME: string, PREFIX?: string, BASE_URL?: string}
 const ONE_MIB = 1024 * 1024
-const PART_SIZE = 100 * ONE_MIB
-const MAX_PUT_SIZE = 100 * ONE_MIB
+const MAX_PUT_SIZE = process.env.NODE_ENV.includes("test") ? 10 * ONE_MIB : 100 * ONE_MIB
 
 function getS3Client () {
   return new S3Client({ endpoint: `https://${CONFIG.OBJECT_STORAGE.ENDPOINT}` })
+}
+
+function getPartSize (stats: Stats) {
+  if (process.env.NODE_ENV.includes("test")) {
+    return 10 * ONE_MIB
+  }
+  // Use parts of 1 GiB if the file is very large (it would take more than 1000 requests at 100 MiB per request)
+  if (stats.size / (100 * ONE_MIB) > 1000) {
+    return 1024 * ONE_MIB
+  }
+  return 100 * ONE_MIB
 }
 
 async function objectStoragePut (options: {filename: string, content: string | ReadStream, bucketInfo: BucketInfo}) {
@@ -43,27 +52,32 @@ async function multiPartUpload (file: {filename: string, path: string}, stats: S
   const { filename, path } = file
   const key = bucketInfo.PREFIX + filename
   const s3Client = getS3Client()
+
   const createMultipartCommand = new CreateMultipartUploadCommand({
     Bucket: bucketInfo.BUCKET_NAME,
     Key: key
   })
   const createResponse = await s3Client.send(createMultipartCommand)
+
+  const fd = await open(path, 'r')
   let partNumber = 1
   const parts: CompletedPart[] = []
-  const fd = await open(path, 'r')
-  for (let start = 0; start < stats.size; start += PART_SIZE) {
+  const partSize = getPartSize(stats)
+  for (let start = 0; start < stats.size; start += partSize) {
     logger.debug('Uploading part %d of file to %s/%s%s', partNumber, bucketInfo.BUCKET_NAME, bucketInfo.PREFIX, file.filename)
-    const stream: ReadStream & {byteLength: number} =
-      createReadStream(
-        path,
-        { fd: fd, autoClose: false, start: start, end: (start + PART_SIZE) - 1 }
-      ) as ReadStream & {byteLength: number}
+
     // The s3 sdk needs to know the length of the http body beforehand, but doesn't support
     // streams with start and end set, so it just tries to stat the file in stream.path.
     // This fails for us because we only want to send part of the file. The stream type
     // is modified so we can set the byteLength here, which s3 detects because array buffers
     // have this field set
-    stream.byteLength = min([ stats.size - start, PART_SIZE ])
+    const stream: ReadStream & {byteLength: number} =
+      createReadStream(
+        path,
+        { fd: fd, autoClose: false, start: start, end: (start + partSize) - 1 }
+      ) as ReadStream & {byteLength: number}
+    // Calculate if the part size is more than what's left over, and in that case use left over bytes for byteLength
+    stream.byteLength = min([ stats.size - start, partSize ])
     const uploadPartCommand = new UploadPartCommand({
       Bucket: bucketInfo.BUCKET_NAME,
       Key: key,
@@ -72,10 +86,12 @@ async function multiPartUpload (file: {filename: string, path: string}, stats: S
       Body: stream
     })
     const uploadResponse = await s3Client.send(uploadPartCommand)
+
     parts.push({ ETag: uploadResponse.ETag, PartNumber: partNumber })
     partNumber += 1
   }
   await close(fd)
+
   const completeUploadCommand = new CompleteMultipartUploadCommand({
     Bucket: bucketInfo.BUCKET_NAME,
     Key: key,
@@ -83,7 +99,7 @@ async function multiPartUpload (file: {filename: string, path: string}, stats: S
     MultipartUpload: { Parts: parts }
   })
   await s3Client.send(completeUploadCommand)
-  logger.debug('Completed in %d parts of file to %s/%s%s', partNumber - 1, bucketInfo.BUCKET_NAME, bucketInfo.PREFIX, file.filename)
+  logger.debug('Completed %s/%s%s in %d parts', bucketInfo.BUCKET_NAME, bucketInfo.PREFIX, file.filename, partNumber - 1)
 }
 
 export async function storeObject (file: {path: string, filename: string}, bucketInfo: BucketInfo) {
@@ -93,6 +109,7 @@ export async function storeObject (file: {path: string, filename: string}, bucke
   if (stats.size > MAX_PUT_SIZE) {
     return await multiPartUpload(file, stats, bucketInfo)
   }
+
   const fileStream = createReadStream(file.path)
   return await objectStoragePut({ filename: file.filename, content: fileStream, bucketInfo })
 }
