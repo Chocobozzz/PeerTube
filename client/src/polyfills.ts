ef44710bb069a73be36cbb00a6173d7502eaf0b5
Chocobozzz <me@florianbigard.com>
2021-08-31 16:25:10 +0200

    Fix player on safari 11

--- client/src/polyfills.ts
+++ client/src/polyfills.ts
@@ -19,6 +19,7 @@
 */
 
 import 'core-js/es/object'
+import 'core-js/es/global-this'
 
 /** IE10 and IE11 requires the following for NgClass support on SVG elements */
 // import 'classlist.js';  // Run `npm install --save classlist.js`.
