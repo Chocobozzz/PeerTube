db9df9a72c67a6ba150620f0f1c0838a3c155786
Chocobozzz <me@florianbigard.com>
2021-10-08 11:57:39 +0200

    Fix local/session storage polyfill

--- client/src/standalone/videos/test-embed.ts
+++ client/src/standalone/videos/test-embed.ts
@@ -15,6 +15,8 @@ window.addEventListener('load', async () => {
     ? `/video-playlists/embed/${elementId}?api=1`
     : `/videos/embed/${elementId}?api=1`
 
+  iframe.sandbox.add('allow-same-origin', 'allow-scripts', 'allow-popups')
+
   const mainElement = document.querySelector('#host')
   mainElement.appendChild(iframe)
 
