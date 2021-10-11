0fe87dc5a1c37d98740bf012e5bac09a21862a89
Chocobozzz <me@florianbigard.com>
2021-10-11 12:00:29 +0200

    Remove unused function

--- client/src/app/helpers/utils/url.ts
+++ client/src/app/helpers/utils/url.ts
@@ -1,19 +1,5 @@
 import { environment } from '../../../environments/environment'
 
-// Thanks: https://stackoverflow.com/questions/901115/how-can-i-get-query-string-values-in-javascript
-function getParameterByName (name: string, url: string) {
-  if (!url) url = window.location.href
-  name = name.replace(/[[\]]/g, '\\$&')
-
-  const regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)')
-  const results = regex.exec(url)
-
-  if (!results) return null
-  if (!results[2]) return ''
-
-  return decodeURIComponent(results[2].replace(/\+/g, ' '))
-}
-
 function getAbsoluteAPIUrl () {
   let absoluteAPIUrl = environment.hmr === true
     ? 'http://localhost:9000'
@@ -64,7 +50,6 @@ function objectToFormData (obj: any, form?: FormData, namespace?: string) {
 }
 
 export {
-  getParameterByName,
   objectToFormData,
   getAbsoluteAPIUrl,
   getAbsoluteEmbedUrl
