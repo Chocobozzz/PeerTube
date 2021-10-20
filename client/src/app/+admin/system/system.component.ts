dbb76162b9cf2d048505e4d6170b694388f68e75
Chocobozzz <me@florianbigard.com>
2021-10-20 10:27:51 +0200

    Admin menu header consistency for system entries

--- client/src/app/+admin/system/system.component.ts
+++ client/src/app/+admin/system/system.component.ts
@@ -1,24 +1,8 @@
 import { Component } from '@angular/core'
-import { UserRight } from '@shared/models'
-import { AuthService } from '@app/core'
 
 @Component({
   templateUrl: './system.component.html',
-  styleUrls: [ './system.component.scss' ]
+  styleUrls: []
 })
 export class SystemComponent {
-
-  constructor (private auth: AuthService) {}
-
-  hasLogsRight () {
-    return this.auth.getUser().hasRight(UserRight.MANAGE_LOGS)
-  }
-
-  hasJobsRight () {
-    return this.auth.getUser().hasRight(UserRight.MANAGE_JOBS)
-  }
-
-  hasDebugRight () {
-    return this.auth.getUser().hasRight(UserRight.MANAGE_DEBUG)
-  }
 }
