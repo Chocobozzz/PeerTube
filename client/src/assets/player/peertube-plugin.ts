cd2fad008a29d5a6b9ee5ea48e51388fa7fc12a0
Chocobozzz <me@florianbigard.com>
2021-11-10 16:14:16 +0100

    Update angular

--- client/src/assets/player/peertube-plugin.ts
+++ client/src/assets/player/peertube-plugin.ts
@@ -224,7 +224,7 @@ class PeerTubePlugin extends Plugin {
   private isTouchEnabled () {
     return ('ontouchstart' in window) ||
       navigator.maxTouchPoints > 0 ||
-      navigator.msMaxTouchPoints > 0
+      (navigator as any).msMaxTouchPoints > 0
   }
 
   private initCaptions () {
