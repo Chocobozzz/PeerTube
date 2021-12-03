451a3f31d7467a91d0091bfecb71c50a48d4a358
Chocobozzz <me@florianbigard.com>
2021-12-03 10:47:16 +0100

    Update plugin selector names

--- shared/models/plugins/client/plugin-selector-id.type.ts
+++ shared/models/plugins/client/plugin-selector-id.type.ts
@@ -1,8 +1,10 @@
-export type PluginSelectorId = 'login-form'
-| 'menu-dropdown-language-item'
-| 'about-instance-features'
-| 'about-instance-statistics'
-| 'about-instance-moderation'
-| 'about-sub-menu-instance'
-| 'about-sub-menu-peertube'
-| 'about-sub-menu-network'
+export type PluginSelectorId =
+  'login-form' |
+  'menu-user-dropdown-language-item' |
+  'about-instance-features' |
+  'about-instance-statistics' |
+  'about-instance-moderation' |
+  'about-menu-instance' |
+  'about-menu-peertube' |
+  'about-menu-network' |
+  'about-instance-other-information'
