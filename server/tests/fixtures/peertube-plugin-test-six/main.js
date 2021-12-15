10788fd5630a3a6656ee22ed4466633935db2f67
lutangar <johan.dufour@gmail.com>
2021-12-15 09:35:17 +0100

    fix plugin storage return value when storing a Json array

--- server/tests/fixtures/peertube-plugin-test-six/main.js
+++ server/tests/fixtures/peertube-plugin-test-six/main.js
@@ -11,9 +11,14 @@ async function register ({
   {
     await storageManager.storeData('superkey', { value: 'toto' })
     await storageManager.storeData('anotherkey', { value: 'toto2' })
+    await storageManager.storeData('storedArrayKey', ['toto', 'toto2'])
 
     const result = await storageManager.getData('superkey')
     logger.info('superkey stored value is %s', result.value)
+
+    const storedArrayValue = await storageManager.getData('storedArrayKey')
+    logger.info('storedArrayKey value type is %s', typeof storedArrayValue)
+    logger.info('storedArrayKey stored value is %s', storedArrayValue.join(', '))
   }
 
   {
