293074ae7920040ede7e01d0aec4dabbeeb864ff
Chocobozzz <me@florianbigard.com>
2021-12-17 15:49:52 +0100

    Fix video upload with big preview

--- server/lib/uploadx.ts
+++ server/lib/uploadx.ts
@@ -2,7 +2,11 @@ import express from 'express'
 import { getResumableUploadPath } from '@server/helpers/upload'
 import { Uploadx } from '@uploadx/core'
 
-const uploadx = new Uploadx({ directory: getResumableUploadPath() })
+const uploadx = new Uploadx({
+  directory: getResumableUploadPath(),
+  // Could be big with thumbnails/previews
+  maxMetadataSize: '10MB'
+})
 uploadx.getUserId = (_, res: express.Response) => res.locals.oauth?.token.user.id
 
 export {
