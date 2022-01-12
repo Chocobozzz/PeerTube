e98ef69d1c8d4e39e45a52b04b3abf5fd1af3b2c
Chocobozzz <me@florianbigard.com>
2022-01-12 09:42:54 +0100

    Move to landscape on mobile fullscreen

--- client/src/assets/player/mobile/peertube-mobile-plugin.ts
+++ client/src/assets/player/mobile/peertube-mobile-plugin.ts
@@ -1,5 +1,5 @@
-import videojs from 'video.js'
 import './peertube-mobile-buttons'
+import videojs from 'video.js'
 
 const Plugin = videojs.getPlugin('plugin')
 
@@ -9,6 +9,23 @@ class PeerTubeMobilePlugin extends Plugin {
     super(player, options)
 
     player.addChild('PeerTubeMobileButtons')
+
+    if (videojs.browser.IS_ANDROID && screen.orientation) {
+      this.handleFullscreenRotation()
+    }
+  }
+
+  private handleFullscreenRotation () {
+    this.player.on('fullscreenchange', () => {
+      if (!this.player.isFullscreen() || this.isPortraitVideo()) return
+
+      screen.orientation.lock('landscape')
+        .catch(err => console.error('Cannot lock screen to landscape.', err))
+    })
+  }
+
+  private isPortraitVideo () {
+    return this.player.videoWidth() < this.player.videoHeight()
   }
 }
 
