af6b45e5d96f37d986c54c1deb6d64e81cd0dbb0
kontrollanten <6680299+kontrollanten@users.noreply.github.com>
2022-02-04 11:11:32 +0100

    client: show private badge in playlists
    closes #2601

--- client/src/app/shared/shared-video-playlist/video-playlist-element-miniature.component.ts
+++ client/src/app/shared/shared-video-playlist/video-playlist-element-miniature.component.ts
@@ -3,7 +3,7 @@ import { AuthService, Notifier, ServerService } from '@app/core'
 import { Video } from '@app/shared/shared-main'
 import { NgbDropdown } from '@ng-bootstrap/ng-bootstrap'
 import { secondsToTime } from '@shared/core-utils'
-import { HTMLServerConfig, VideoPlaylistElementType, VideoPlaylistElementUpdate } from '@shared/models'
+import { HTMLServerConfig, VideoPlaylistElementType, VideoPlaylistElementUpdate, VideoPrivacy } from '@shared/models'
 import { VideoPlaylistElement } from './video-playlist-element.model'
 import { VideoPlaylist } from './video-playlist.model'
 import { VideoPlaylistService } from './video-playlist.service'
@@ -49,6 +49,10 @@ export class VideoPlaylistElementMiniatureComponent implements OnInit {
 
   ngOnInit (): void {
     this.serverConfig = this.serverService.getHTMLConfig()
+  }
+
+  isVideoPrivate () {
+    return this.playlistElement.video.privacy.id === VideoPrivacy.PRIVATE
   }
 
   isUnavailable (e: VideoPlaylistElement) {
