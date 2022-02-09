400ed2ab4155689d1695cfbce9358f9ca3a5da60
Chocobozzz <me@florianbigard.com>
2022-02-09 11:47:22 +0100

    Add log on user plugin auth conflict

--- server/lib/auth/oauth-model.ts
+++ server/lib/auth/oauth-model.ts
@@ -98,7 +98,14 @@ async function getUser (usernameOrEmail?: string, password?: string, bypassLogin
     // Then we just go through a regular login process
     if (user.pluginAuth !== null) {
       // This user does not belong to this plugin, skip it
-      if (user.pluginAuth !== bypassLogin.pluginName) return null
+      if (user.pluginAuth !== bypassLogin.pluginName) {
+        logger.info(
+          'Cannot bypass oauth login by plugin %s because %s has another plugin auth method (%s).',
+          bypassLogin.pluginName, bypassLogin.user.email, user.pluginAuth
+        )
+
+        return null
+      }
 
       checkUserValidityOrThrow(user)
 
