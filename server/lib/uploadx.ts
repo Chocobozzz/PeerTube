77d0ae7b201264423d81e1019139eac3d7716536
Chocobozzz <me@florianbigard.com>
2022-04-19 11:44:03 +0200

    Fix 2 hours limit on uploads

--- server/lib/uploadx.ts
+++ server/lib/uploadx.ts
@@ -4,8 +4,12 @@ import { Uploadx } from '@uploadx/core'
 
 const uploadx = new Uploadx({
   directory: getResumableUploadPath(),
+
+  expiration: { maxAge: undefined, rolling: true },
+
   // Could be big with thumbnails/previews
   maxMetadataSize: '10MB',
+
   userIdentifier: (_, res: express.Response) => {
     if (!res.locals.oauth) return undefined
 
