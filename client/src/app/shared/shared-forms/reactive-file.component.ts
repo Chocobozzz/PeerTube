8a2166c9bfa452ce707740d99c64d668cd7cedce
Chocobozzz <me@florianbigard.com>
2022-06-01 08:25:41 +0200

    Fix typo

--- client/src/app/shared/shared-forms/reactive-file.component.ts
+++ client/src/app/shared/shared-forms/reactive-file.component.ts
@@ -57,7 +57,7 @@ export class ReactiveFileComponent implements OnInit, ControlValueAccessor {
 
       const extension = '.' + file.name.split('.').pop()
       if (this.extensions.includes(extension.toLowerCase()) === false) {
-        const message = $localize`PeerTube cannot handle this kind of file. Accepted extensions are ${this.allowedExtensionsMessage}}.`
+        const message = $localize`PeerTube cannot handle this kind of file. Accepted extensions are ${this.allowedExtensionsMessage}.`
         this.notifier.error(message)
 
         return
