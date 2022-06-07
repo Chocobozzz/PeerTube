6f1f4a60fee1a7bf69ed07974135358eb36274ed
durgaraj726 <durgaraj726@gmail.com>
2022-06-07 12:08:45 +0545

    fixes video not played from defined parameters in embed service

--- server/controllers/services.ts
+++ server/controllers/services.ts
@@ -4,6 +4,7 @@ import { asyncMiddleware, oembedValidator } from '../middlewares'
 import { accountNameWithHostGetValidator } from '../middlewares/validators'
 import { MChannelSummary } from '@server/types/models'
 import { escapeHTML } from '@shared/core-utils/renderer'
+import { logger } from '@server/helpers/logger'
 
 const servicesRouter = express.Router()
 
@@ -48,10 +49,13 @@ function generatePlaylistOEmbed (req: express.Request, res: express.Response) {
 function generateVideoOEmbed (req: express.Request, res: express.Response) {
   const video = res.locals.videoAll
 
+  const regex = /\?[^?]*$/
+  const params = regex.test(req.query.url) ? req.query.url.match(regex) : ''
+
   const json = buildOEmbed({
     channel: video.VideoChannel,
     title: video.name,
-    embedPath: video.getEmbedStaticPath(),
+    embedPath: video.getEmbedStaticPath() + params,
     previewPath: video.getPreviewStaticPath(),
     previewSize: PREVIEWS_SIZE,
     req
