3031971ec21ed6400d3b73d1f7e00b44d1617667
Chocobozzz <me@florianbigard.com>
2022-06-20 15:16:26 +0200

    Less complicated markdown textarea CSS

--- client/src/app/shared/shared-forms/markdown-textarea.component.ts
+++ client/src/app/shared/shared-forms/markdown-textarea.component.ts
@@ -24,6 +24,7 @@ import { Video } from '@shared/models'
 export class MarkdownTextareaComponent implements ControlValueAccessor, OnInit {
   @Input() content = ''
 
+  @Input() formError: string
   @Input() classes: string[] | { [klass: string]: any[] | any } = []
 
   @Input() textareaMaxWidth = '100%'
@@ -93,6 +94,8 @@ export class MarkdownTextareaComponent implements ControlValueAccessor, OnInit {
   }
 
   onMaximizeClick () {
+    if (this.disabled) return
+
     this.isMaximized = !this.isMaximized
 
     // Make sure textarea have the focus
