f7ce623db1225b7b6f8383c33cab9515b36907fe
Chocobozzz <me@florianbigard.com>
2022-07-13 11:21:19 +0200

    Don't log error on static 404

--- server/controllers/static.ts
+++ server/controllers/static.ts
@@ -1,3 +1,4 @@
+import { HttpStatusCode } from '@shared/models'
 import cors from 'cors'
 import express from 'express'
 import { CONFIG } from '../initializers/config'
@@ -11,28 +12,48 @@ staticRouter.use(cors())
 // Videos path for webseed
 staticRouter.use(
   STATIC_PATHS.WEBSEED,
-  express.static(CONFIG.STORAGE.VIDEOS_DIR, { fallthrough: false }) // 404 because we don't have this video
+  express.static(CONFIG.STORAGE.VIDEOS_DIR, { fallthrough: false }),
+  handleStaticError
 )
 staticRouter.use(
   STATIC_PATHS.REDUNDANCY,
-  express.static(CONFIG.STORAGE.REDUNDANCY_DIR, { fallthrough: false }) // 404 because we don't have this video
+  express.static(CONFIG.STORAGE.REDUNDANCY_DIR, { fallthrough: false }),
+  handleStaticError
 )
 
 // HLS
 staticRouter.use(
   STATIC_PATHS.STREAMING_PLAYLISTS.HLS,
-  express.static(HLS_STREAMING_PLAYLIST_DIRECTORY, { fallthrough: false }) // 404 if the file does not exist
+  express.static(HLS_STREAMING_PLAYLIST_DIRECTORY, { fallthrough: false }),
+  handleStaticError
 )
 
 // Thumbnails path for express
 const thumbnailsPhysicalPath = CONFIG.STORAGE.THUMBNAILS_DIR
 staticRouter.use(
   STATIC_PATHS.THUMBNAILS,
-  express.static(thumbnailsPhysicalPath, { maxAge: STATIC_MAX_AGE.SERVER, fallthrough: false }) // 404 if the file does not exist
+  express.static(thumbnailsPhysicalPath, { maxAge: STATIC_MAX_AGE.SERVER, fallthrough: false }),
+  handleStaticError
 )
 
 // ---------------------------------------------------------------------------
 
 export {
   staticRouter
+}
+
+// ---------------------------------------------------------------------------
+
+function handleStaticError (err: any, req: express.Request, res: express.Response, next: express.NextFunction) {
+  const message = err.message || ''
+
+  if (message.includes('ENOENT')) {
+    return res.fail({
+      status: err.status || HttpStatusCode.INTERNAL_SERVER_ERROR_500,
+      message: err.message,
+      type: err.name
+    })
+  }
+
+  return next(err)
 }
