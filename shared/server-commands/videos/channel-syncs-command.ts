197066014647efc4bee3792ca91268c32908d483
Florent <florent.git@zeteo.me>
2022-07-29 09:28:19 +0200

    Add command for VideoChannelSync (#754)

---
+++ shared/server-commands/videos/channel-syncs-command.ts
@@ -0,0 +1,67 @@
+import { HttpStatusCode, ResultList, VideoChannelCreateResult, VideoChannelSync, VideoChannelSyncCreate } from "@shared/models"
+import { pick } from "lodash"
+import { unwrapBody } from "../requests"
+import { AbstractCommand, OverrideCommandOptions } from "../shared"
+
+export class ChannelsSyncCommand extends AbstractCommand {
+  private static readonly API_PATH = '/api/v1/video-channel-syncs'
+  listByAccount (options: OverrideCommandOptions & {
+    accountName: string
+    start?: number
+    count?: number
+    sort?: string
+  }) {
+    const { accountName, sort = 'createdAt' } = options
+    const path = `/api/v1/accounts/${accountName}/video-channel-syncs`
+    return this.getRequestBody<ResultList<VideoChannelSync>>({
+      ...options,
+
+      path,
+      query: { sort, ...pick(options, [ 'start', 'count', 'withStats', 'search' ]) },
+      implicitToken: false,
+      defaultExpectedStatus: HttpStatusCode.OK_200
+    })
+  }
+
+  async create (options: OverrideCommandOptions & {
+    attributes: VideoChannelSyncCreate
+  }) {
+    const response = this.postBodyRequest({
+      path: ChannelsSyncCommand.API_PATH,
+      ...options,
+      fields: options.attributes,
+      implicitToken: true,
+      defaultExpectedStatus: HttpStatusCode.OK_200
+    })
+    const body = await unwrapBody<{ videoChannelSync: VideoChannelCreateResult }>(response)
+    return body.videoChannelSync
+  }
+
+  delete (options: OverrideCommandOptions & {
+    channelSyncId: number
+  }) {
+    const path = `${ChannelsSyncCommand.API_PATH}/${options.channelSyncId}`
+
+    return this.deleteRequest({
+      ...options,
+
+      path,
+      implicitToken: true,
+      defaultExpectedStatus: HttpStatusCode.NO_CONTENT_204
+    })
+  }
+
+  syncChannel (options: OverrideCommandOptions & {
+    channelSyncId: number
+  }) {
+    const path = `${ChannelsSyncCommand.API_PATH}/${options.channelSyncId}/sync`
+
+    return this.postBodyRequest({
+      ...options,
+
+      path,
+      implicitToken: true,
+      defaultExpectedStatus: HttpStatusCode.NO_CONTENT_204
+    })
+  }
+}
