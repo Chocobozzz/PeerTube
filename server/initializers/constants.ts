db312c7023bb8263741255f8af1c8a6aef5f88f7
Chocobozzz <me@florianbigard.com>
2022-09-13 15:22:02 +0200

    Fix old DB enum names

--- server/initializers/constants.ts
+++ server/initializers/constants.ts
@@ -25,7 +25,7 @@ import { CONFIG, registerConfigChangedHandler } from './config'
 
 // ---------------------------------------------------------------------------
 
-const LAST_MIGRATION_VERSION = 735
+const LAST_MIGRATION_VERSION = 740
 
 // ---------------------------------------------------------------------------
 
