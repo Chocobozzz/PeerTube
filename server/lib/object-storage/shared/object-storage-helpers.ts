5617654b3989de57d17136df4ad1f88ef4723c26
Kinuseka <realkingseeker1089@gmail.com>
2023-01-02 00:54:17 +0800

    reassign variable, parse json

--- server/lib/object-storage/shared/object-storage-helpers.ts
+++ server/lib/object-storage/shared/object-storage-helpers.ts
@@ -205,7 +205,7 @@ async function updateObjectBucketPolicy (options: {
   if (!bucketPolicyResponse.Policy){
     await createPolicy({bucketInfo: bucketInfo})
     logger.debug('Reattempting to fetch bucket policy')
-    bucketPolicyResponse = (await getbucketPolicy({bucketInfo: bucketInfo})).Policy
+    var bucketPolicyResponse = JSON.parse((await getbucketPolicy({bucketInfo: bucketInfo})).Policy)
     if (!bucketPolicyResponse.Policy) {
       throw new Error('Cannot fetch bucket policy')
     }
@@ -235,7 +235,7 @@ async function updateObjectBucketPolicyPrefix (options: {
   if (!bucketPolicyResponse.Policy){
     await createPolicy({bucketInfo: bucketInfo})
     logger.debug('Reattempting to fetch bucket policy')
-    bucketPolicyResponse = (await getbucketPolicy({bucketInfo: bucketInfo})).Policy
+    var bucketPolicyResponse = JSON.parse((await getbucketPolicy({bucketInfo: bucketInfo})).Policy)
     if (!bucketPolicyResponse.Policy) {
       throw new Error('Cannot fetch bucket policy')
     }
