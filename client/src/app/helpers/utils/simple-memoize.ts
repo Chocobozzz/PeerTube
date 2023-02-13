57d64f30e30b57ba5dc035fcaf38895d775412bd
Chocobozzz <me@florianbigard.com>
2023-02-13 14:27:35 +0100

    Improve channel's avatar display performance

---
+++ client/src/app/helpers/utils/simple-memoize.ts
@@ -0,0 +1,49 @@
+/**
+ *
+ * Simple memoize only support methods that accept 0 or 1 argument
+ * You can easily use it adding @SimpleMemoize just above the method name
+ *
+ */
+
+export function SimpleMemoize () {
+  const store = new Map()
+
+  return (_target: object, _propertyKey: string, descriptor: TypedPropertyDescriptor<any>) => {
+    if (descriptor.value != null) {
+      descriptor.value = getNewFunction(descriptor.value, store)
+      return
+    }
+
+    throw new Error('Only put a Memoize() decorator on a method accessor.')
+  }
+}
+
+function getNewFunction (originalMethod: () => void, store: Map<any, any>) {
+  return function (this: any, ...args: any[]) {
+    if (args.length > 1) {
+      throw new Error('Simple memoize only support 0 or 1 argument')
+    }
+
+    let returnedValue: any
+
+    if (args.length > 0) {
+      const hashKey = args[0]
+
+      if (store.has(hashKey)) {
+        returnedValue = store.get(hashKey)
+      } else {
+        returnedValue = originalMethod.apply(this, args)
+        store.set(hashKey, returnedValue)
+      }
+    } else {
+      if (store.has(this)) {
+        returnedValue = store.get(this)
+      } else {
+        returnedValue = originalMethod.apply(this, args)
+        store.set(this, returnedValue)
+      }
+    }
+
+    return returnedValue
+  }
+}
