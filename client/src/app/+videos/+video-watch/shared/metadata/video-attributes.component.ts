4265d90b0061399e23b816e3880ee1be47ead96f
Chocobozzz <me@florianbigard.com>
2023-03-10 11:10:16 +0100

    Add ability for plugins to add metadata

--- client/src/app/+videos/+video-watch/shared/metadata/video-attributes.component.ts
+++ client/src/app/+videos/+video-watch/shared/metadata/video-attributes.component.ts
@@ -1,13 +1,34 @@
-import { Component, Input } from '@angular/core'
+import { Component, Input, OnInit } from '@angular/core'
+import { HooksService } from '@app/core'
 import { VideoDetails } from '@app/shared/shared-main'
+
+type PluginMetadata = {
+  label: string
+
+  value?: string
+  safeHTML?: string
+}
 
 @Component({
   selector: 'my-video-attributes',
   templateUrl: './video-attributes.component.html',
   styleUrls: [ './video-attributes.component.scss' ]
 })
-export class VideoAttributesComponent {
+export class VideoAttributesComponent implements OnInit {
   @Input() video: VideoDetails
+
+  pluginMetadata: PluginMetadata[] = []
+
+  constructor (private hooks: HooksService) { }
+
+  async ngOnInit () {
+    this.pluginMetadata = await this.hooks.wrapFunResult(
+      this.buildPluginMetadata.bind(this),
+      { video: this.video },
+      'video-watch',
+      'filter:video-watch.video-plugin-metadata.result'
+    )
+  }
 
   getVideoHost () {
     return this.video.channel.host
@@ -17,5 +38,12 @@ export class VideoAttributesComponent {
     if (!this.video || Array.isArray(this.video.tags) === false) return []
 
     return this.video.tags
+  }
+
+  // Used for plugin hooks
+  private buildPluginMetadata (_options: {
+    video: VideoDetails
+  }): PluginMetadata[] {
+    return []
   }
 }
