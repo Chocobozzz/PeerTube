f86ff3a02642263ac64db5d5fba4059db0092561
Chocobozzz <me@florianbigard.com>
2023-05-17 16:19:30 +0200

    Prevent uninstall error on install error

--- server/lib/plugins/plugin-manager.ts
+++ server/lib/plugins/plugin-manager.ts
@@ -370,15 +370,17 @@ export class PluginManager implements ServerHook {
     } catch (rootErr) {
       logger.error('Cannot install plugin %s, removing it...', toInstall, { err: rootErr })
 
-      try {
-        await this.uninstall({ npmName })
-      } catch (err) {
-        logger.error('Cannot uninstall plugin %s after failed installation.', toInstall, { err })
-
+      if (npmName) {
         try {
-          await removeNpmPlugin(npmName)
+          await this.uninstall({ npmName })
         } catch (err) {
-          logger.error('Cannot remove plugin %s after failed installation.', toInstall, { err })
+          logger.error('Cannot uninstall plugin %s after failed installation.', toInstall, { err })
+
+          try {
+            await removeNpmPlugin(npmName)
+          } catch (err) {
+            logger.error('Cannot remove plugin %s after failed installation.', toInstall, { err })
+          }
         }
       }
 
