83e1e4dfb2c09f9291e7ae64f2b2f5440c4c179c
Chocobozzz <me@florianbigard.com>
2023-12-08 11:16:43 +0100

    Fix backward compat upgrade script

--- scripts/upgrade.sh
+++ scripts/upgrade.sh
@@ -4,4 +4,5 @@ set -eu
 
 # Backward path compatibility now upgrade.sh is in dist/scripts since v6
 
-/bin/sh ../dist/scripts/upgrade.sh $1
+/bin/sh ../dist/scripts/upgrade.sh ${1:-/var/www/peertube}
+
