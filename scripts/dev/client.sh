1048d1054b99f1af1bc6107a49900add76b36c8c
Chocobozzz <me@florianbigard.com>
2024-03-15 16:49:55 +0100

    Fix dev:client command

--- scripts/dev/client.sh
+++ scripts/dev/client.sh
@@ -9,11 +9,13 @@ if [ ! -z ${2+x} ] && [ "$2" = "--ar-locale" ]; then
 fi
 
 clientCommand="cd client && node --max_old_space_size=4096 node_modules/.bin/ng serve --proxy-config proxy.config.json --hmr --configuration $clientConfiguration --host 0.0.0.0 --disable-host-check --port 3000"
-serverCommand="npm run build:server && NODE_ENV=dev node dist/server"
+serverCommand=" NODE_ENV=dev node dist/server"
 
 if [ ! -z ${1+x} ] && [ "$1" = "--skip-server" ]; then
   eval $clientCommand
 else
+  npm run build:server
+
   node node_modules/.bin/concurrently -k \
     "$clientCommand" \
     "$serverCommand"
