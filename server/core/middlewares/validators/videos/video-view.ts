2ff7dee3f120d923f732e30671e2c54030a7c471
kontrollanten <6680299+kontrollanten@users.noreply.github.com>
2024-03-25 06:18:37 +0100

    fix(server/video-view): log invalid currentTime req
    relates to #6285

--- server/core/middlewares/validators/videos/video-view.ts
+++ server/core/middlewares/validators/videos/video-view.ts
@@ -6,6 +6,7 @@ import { getCachedVideoDuration } from '@server/lib/video.js'
 import { LocalVideoViewerModel } from '@server/models/view/local-video-viewer.js'
 import { isIdValid, toIntOrNull } from '../../../helpers/custom-validators/misc.js'
 import { areValidationErrors, doesVideoExist, isValidVideoIdParam } from '../shared/index.js'
+import { logger } from '@server/helpers/logger.js'
 
 const getVideoLocalViewerValidator = [
   param('localViewerId')
@@ -43,6 +44,8 @@ const videoViewValidator = [
     const { duration } = await getCachedVideoDuration(video.id)
 
     if (!isVideoTimeValid(req.body.currentTime, duration)) {
+      logger.warn('Current time is invalid', { duration, currentTime: req.body.currentTime, videoId: video.id })
+
       return res.fail({
         status: HttpStatusCode.BAD_REQUEST_400,
         message: 'Current time is invalid'
