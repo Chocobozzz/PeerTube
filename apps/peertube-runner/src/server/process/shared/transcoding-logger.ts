0794fe2ac1e2a835bdfd58350a9de0fb04e31238
Chocobozzz <me@florianbigard.com>
2024-04-03 09:17:45 +0200

    Fix runner ffmpeg logger

--- apps/peertube-runner/src/server/process/shared/transcoding-logger.ts
+++ apps/peertube-runner/src/server/process/shared/transcoding-logger.ts
@@ -1,10 +1,19 @@
+import { LogFn } from 'pino'
 import { logger } from '../../../shared/index.js'
 
 export function getTranscodingLogger () {
   return {
-    info: logger.info.bind(logger),
-    debug: logger.debug.bind(logger),
-    warn: logger.warn.bind(logger),
-    error: logger.error.bind(logger)
+    info: buildWinstonLogger(logger.info.bind(logger)),
+    debug: buildWinstonLogger(logger.debug.bind(logger)),
+    warn: buildWinstonLogger(logger.warn.bind(logger)),
+    error: buildWinstonLogger(logger.error.bind(logger))
+  }
+}
+
+function buildWinstonLogger (log: LogFn) {
+  return (arg1: string, arg2?: object) => {
+    if (arg2) return log(arg2, arg1)
+
+    return log(arg1)
   }
 }
