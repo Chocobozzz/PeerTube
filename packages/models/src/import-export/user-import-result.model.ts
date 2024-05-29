michaelperna/9ten

Implement auto tag on comments and videos
 * Comments and videos can be automatically tagged using core rules or
   watched word lists
 * These tags can be used to automatically filter videos and comments
 * Introduce a new video comment policy where comments must be approved
   first
 * Comments may have to be approved if the user auto block them using
   core rules or watched word lists
 * Implement FEP-5624 to federate reply control policies

packages/models/src/import-export/user-import-result.model.ts
@@ -18,5 +18,8 @@ export interface UserImportResultSummary {
     userSettings: Summary
 
     userVideoHistory: Summary
+
+    watchedWordsLists: Summary
+    reviewCommentsTagsPolicies: Summary
   }
 }

Chocobozzz/PeerTube

packages/models/src/import-export/user-import-result.model.ts
@@ -18,5 +18,8 @@ export interface UserImportResultSummary {
     userSettings: Summary
 
     userVideoHistory: Summary
+
+    watchedWordsLists: Summary
+    commentAutoTagPolicies: Summary
   }
 }