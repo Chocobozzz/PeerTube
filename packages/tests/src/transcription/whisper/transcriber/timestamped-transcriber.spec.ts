fb254cb4f706d2f1bbeb1064b8278b037ced8c35
lutangar <johan@larriereguichet.fr>
2024-06-11 20:25:44 +0200

    chore(transcription): raise timeout some more

--- packages/tests/src/transcription/whisper/transcriber/timestamped-transcriber.spec.ts
+++ packages/tests/src/transcription/whisper/transcriber/timestamped-transcriber.spec.ts
@@ -99,7 +99,7 @@ describe('Linto timestamped Whisper transcriber', function () {
   })
 
   it('Should produce a text transcript similar to openai-whisper implementation', async function () {
-    this.timeout(5 * 1000 * 60)
+    this.timeout(6 * 1000 * 60)
     const transcribeArgs: WhisperTranscribeArgs = {
       mediaFilePath: frVideoPath,
       model: await TranscriptionModel.fromPath(join(modelsDirectory, 'tiny.pt')),
