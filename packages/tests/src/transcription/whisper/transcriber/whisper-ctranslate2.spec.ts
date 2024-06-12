b2f57345ad4073d1b789965d50f45e7dfc5449bb
lutangar <johan@larriereguichet.fr>
2024-06-12 10:29:31 +0200

    chore(transcription): raise timeout some more

--- packages/tests/src/transcription/whisper/transcriber/whisper-ctranslate2.spec.ts
+++ packages/tests/src/transcription/whisper/transcriber/whisper-ctranslate2.spec.ts
@@ -106,7 +106,7 @@ describe('Whisper CTranslate2 transcriber', function () {
   })
 
   it('Should produce a text transcript similar to openai-whisper implementation', async function () {
-    this.timeout(6 * 1000 * 60)
+    this.timeout(7 * 1000 * 60)
     const transcribeArgs: WhisperTranscribeArgs = {
       mediaFilePath: frVideoPath,
       language: 'fr',
