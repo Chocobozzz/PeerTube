348161da6e4da95b9dae10e12900ebea10eb3637
Chocobozzz <me@florianbigard.com>
2024-12-02 08:57:15 +0100

    Fix subtitle nav download

--- client/src/app/shared/shared-video-miniature/download/video-generate-download.component.ts
+++ client/src/app/shared/shared-video-miniature/download/video-generate-download.component.ts
@@ -1,11 +1,9 @@
-import { KeyValuePipe, NgClass, NgFor, NgIf, NgTemplateOutlet } from '@angular/common'
+import { NgIf } from '@angular/common'
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core'
 import { FormsModule } from '@angular/forms'
 import { PeertubeCheckboxComponent } from '@app/shared/shared-forms/peertube-checkbox.component'
 import { VideoService } from '@app/shared/shared-main/video/video.service'
-import {
-  NgbTooltip
-} from '@ng-bootstrap/ng-bootstrap'
+import { NgbTooltip } from '@ng-bootstrap/ng-bootstrap'
 import { getResolutionAndFPSLabel, maxBy } from '@peertube/peertube-core-utils'
 import { VideoFile, VideoResolution, VideoSource } from '@peertube/peertube-models'
 import { videoRequiresFileToken } from '@root-helpers/video'
@@ -23,11 +21,7 @@ import { VideoDetails } from '../../shared-main/video/video-details.model'
     FormsModule,
     GlobalIconComponent,
     PeertubeCheckboxComponent,
-    NgFor,
-    KeyValuePipe,
     NgbTooltip,
-    NgTemplateOutlet,
-    NgClass,
     BytesPipe
   ]
 })
