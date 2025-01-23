bb7be7a9ca2a6a7269d6f7fb1394f9d0fd75a72c
kontrollanten <6680299+kontrollanten@users.noreply.github.com>
2025-01-23 08:42:53 +0100

    move plugin actions to client-action.model.ts

--- packages/models/src/plugins/client/client-hook.model.ts
+++ packages/models/src/plugins/client/client-hook.model.ts
@@ -212,17 +212,3 @@ export type ClientHookName = keyof typeof clientHookObject
 export interface ClientHook {
   runHook <T> (hookName: ClientHookName, result?: T, params?: any): Promise<T>
 }
-
-export const clientDoActionObject = {
-  'admin-abuse-list:load-data': true,
-  'application:increment-loader': true,
-  'application:decrement-loader': true,
-  'admin-users-list:load-data': true,
-  'admin-video-comment-list:load-data': true,
-  'video-watch-comment-list:load-data': true
-}
-
-export type ClientDoActionName = keyof typeof clientDoActionObject
-
-export type ClientDoActionCallback = () => Promise<any>
-export type ClientDoAction = (actionName: ClientDoActionName) => Promise<any>
