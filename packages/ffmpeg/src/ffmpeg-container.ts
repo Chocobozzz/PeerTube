8f87a6cd9fd533d2d6235bf7607f05c4ddf319e1
Chocobozzz <me@florianbigard.com>
2025-04-11 10:30:16 +0200

    Fix audio only download

--- packages/ffmpeg/src/ffmpeg-container.ts
+++ packages/ffmpeg/src/ffmpeg-container.ts
@@ -17,16 +17,22 @@ export class FFmpegContainer {
   }) {
     const { inputs, output, logError, coverPath } = options
 
-    this.commandWrapper.buildCommand(inputs)
-      .outputOption('-c copy')
-      .outputOption('-movflags frag_keyframe+empty_moov')
-      .format('mp4')
-      .output(output)
+    const command = this.commandWrapper.buildCommand(inputs)
+
+    for (let i = 0; i < inputs.length; i++) {
+      command.outputOption('-map ' + i)
+    }
 
     if (coverPath) {
-      this.commandWrapper.getCommand()
-        .addInput(coverPath)
+      command.addInput(coverPath)
+      command.outputOption('-map ' + inputs.length)
     }
+
+    command.outputOption('-c copy')
+      .outputOption('-movflags frag_every_frame+empty_moov')
+      .outputOption('-min_frag_duration 5M') // 5 seconds
+      .format('mp4')
+      .output(output)
 
     return this.commandWrapper.runCommand({ silent: !logError })
   }
