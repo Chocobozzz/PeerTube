04245f9dc175ac7751bcc68a9c3a15f1a9634e6d
Chocobozzz <me@florianbigard.com>
2025-07-30 14:25:30 +0200

    Revert "Upgrade webfinger lib to 2.8.1"
    This reverts commit 1967546cab95d146e60af871d43e6a4990967996.
    Another bug that must be fixed first: https://github.com/silverbucket/webfinger.js/issues/116

--- server/core/lib/activitypub/actors/webfinger.ts
+++ server/core/lib/activitypub/actors/webfinger.ts
@@ -1,18 +1,19 @@
+import WebFinger from 'webfinger.js'
+import { WebFingerData } from '@peertube/peertube-models'
 import { isProdInstance } from '@peertube/peertube-node-utils'
 import { isActivityPubUrlValid } from '@server/helpers/custom-validators/activitypub/misc.js'
 import { REQUEST_TIMEOUTS, WEBSERVER } from '@server/initializers/constants.js'
 import { ActorModel } from '@server/models/actor/actor.js'
 import { MActorFull } from '@server/types/models/index.js'
-import WebFinger from 'webfinger.js'
 
-// eslint-disable-next-line new-cap
 const webfinger = new WebFinger({
+  webfist_fallback: false,
   tls_only: isProdInstance(),
   uri_fallback: false,
   request_timeout: REQUEST_TIMEOUTS.DEFAULT
 })
 
-export async function loadActorUrlOrGetFromWebfinger (uriArg: string) {
+async function loadActorUrlOrGetFromWebfinger (uriArg: string) {
   // Handle strings like @toto@example.com
   const uri = uriArg.startsWith('@') ? uriArg.slice(1) : uriArg
 
@@ -30,15 +31,37 @@ export async function loadActorUrlOrGetFromWebfinger (uriArg: string) {
   return getUrlFromWebfinger(uri)
 }
 
-export async function getUrlFromWebfinger (uri: string) {
-  const { object } = await webfinger.lookup(uri)
+async function getUrlFromWebfinger (uri: string) {
+  const webfingerData: WebFingerData = await webfingerLookup(uri)
+  return getLinkOrThrow(webfingerData)
+}
 
-  if (Array.isArray(object.links) === false) throw new Error('WebFinger links is not an array.')
+// ---------------------------------------------------------------------------
 
-  const selfLink = object.links.find(l => l.rel === 'self')
-  if (selfLink === undefined || isActivityPubUrlValid(selfLink.href as string) === false) {
+export {
+  getUrlFromWebfinger,
+  loadActorUrlOrGetFromWebfinger
+}
+
+// ---------------------------------------------------------------------------
+
+function getLinkOrThrow (webfingerData: WebFingerData) {
+  if (Array.isArray(webfingerData.links) === false) throw new Error('WebFinger links is not an array.')
+
+  const selfLink = webfingerData.links.find(l => l.rel === 'self')
+  if (selfLink === undefined || isActivityPubUrlValid(selfLink.href) === false) {
     throw new Error('Cannot find self link or href is not a valid URL.')
   }
 
-  return selfLink.href as string
+  return selfLink.href
+}
+
+function webfingerLookup (handle: string) {
+  return new Promise<WebFingerData>((res, rej) => {
+    webfinger.lookup(handle, (err, p) => {
+      if (err) return rej(err)
+
+      return res(p.object)
+    })
+  })
 }
