3316795a592599f20545723ef75a814387f3eeb7
Gianantonio Pini <63844628+gianantoniopini@users.noreply.github.com>
2025-12-05 17:23:45 +0100

    feat(colorpicker): add initial version of new component

---
+++ client/src/app/shared/shared-forms/peertube-color-picker.component.ts
@@ -0,0 +1,41 @@
+import { Component, forwardRef, input, model } from '@angular/core'
+import { ControlValueAccessor, FormsModule, NG_VALUE_ACCESSOR } from '@angular/forms'
+import { ColorPickerModule } from 'primeng/colorpicker'
+
+@Component({
+  selector: 'my-peertube-color-picker',
+  templateUrl: './peertube-color-picker.component.html',
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => PeertubeColorPickerComponent),
+      multi: true
+    }
+  ],
+  imports: [ FormsModule, ColorPickerModule ]
+})
+export class PeertubeColorPickerComponent implements ControlValueAccessor {
+  readonly inputId = input.required<string>()
+
+  readonly value = model('')
+
+  propagateChange = (_: any) => {
+    // empty
+  }
+
+  writeValue (value: string) {
+    this.value.set(value)
+  }
+
+  registerOnChange (fn: (_: any) => void) {
+    this.propagateChange = fn
+  }
+
+  registerOnTouched () {
+    // Unused
+  }
+
+  onModelChange () {
+    this.propagateChange(this.value())
+  }
+}
