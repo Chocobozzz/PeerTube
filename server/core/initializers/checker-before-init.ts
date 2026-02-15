0d83405cae42fd14a45dc167ca19388c7f073fa6
root <root@peertube.home.lan>
2026-02-15 19:56:55 +0000

    Remove force_path_style config dependency

--- server/core/initializers/checker-before-init.ts
+++ server/core/initializers/checker-before-init.ts
@@ -189,7 +189,6 @@ export function checkMissedConfig () {
     'object_storage.enabled',
     'object_storage.endpoint',
     'object_storage.region',
-    'object_storage.force_path_style',
     'object_storage.upload_acl.public',
     'object_storage.upload_acl.private',
     'object_storage.proxy.proxify_private_files',
