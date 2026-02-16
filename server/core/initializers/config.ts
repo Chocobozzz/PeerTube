84292c4252acd9828e11c83fb25c1427714e65a8
root <root@peertube.home.lan>
2026-02-16 19:14:56 +0000

    Revert "Remove force_path_style config dependency"
    This reverts commit 0d83405cae42fd14a45dc167ca19388c7f073fa6.

--- server/core/initializers/config.ts
+++ server/core/initializers/config.ts
@@ -264,8 +264,7 @@ const CONFIG = {
     MAX_REQUEST_ATTEMPTS: config.get<number>('object_storage.max_request_attempts'),
     ENDPOINT: config.get<string>('object_storage.endpoint'),
     REGION: config.get<string>('object_storage.region'),
-    // Path-style requests are intentionally disabled in this fork.
-    FORCE_PATH_STYLE: false,
+    FORCE_PATH_STYLE: config.get<boolean>('object_storage.force_path_style'),
     UPLOAD_ACL: {
       PUBLIC: config.get<string>('object_storage.upload_acl.public'),
       PRIVATE: config.get<string>('object_storage.upload_acl.private')
