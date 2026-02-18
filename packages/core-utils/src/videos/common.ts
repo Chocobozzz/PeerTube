44f8f0e56ca7fbc5041eb14fd90bd36dfd290b3d
oliwierjaszczyszyn <h@h>
2026-02-18 23:20:51 +0100

    Opt in for i18n parameter instead
    Gonna do some hit and miss here, for some reason I cannot get the frontend to work

--- packages/core-utils/src/videos/common.ts
+++ packages/core-utils/src/videos/common.ts
@@ -53,7 +53,7 @@ export function getResolutionLabel (options: {
 }) {
   const { height, width } = options
 
-  if (options.resolution === 0) return $localize`Audio only`
+  if (options.resolution === 0) return 'Audio only'
 
   let resolution = options.resolution
 
