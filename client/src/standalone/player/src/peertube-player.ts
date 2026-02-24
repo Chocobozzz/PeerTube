ace4eb68cdf1da60e08c7e7dc760cbcdef381ad7
Chocobozzz <me@florianbigard.com>
2026-02-24 08:59:00 +0100

    Support x3.0 playback speed for non-premium users :)

--- client/src/standalone/player/src/peertube-player.ts
+++ client/src/standalone/player/src/peertube-player.ts
@@ -468,7 +468,7 @@ export class PeerTubePlayer {
       preload: 'none' as 'none',
 
       inactivityTimeout: this.options.inactivityTimeout,
-      playbackRates: [ 0.25, 0.5, 0.75, 1, 1.25, 1.5, 1.75, 2 ],
+      playbackRates: [ 0.25, 0.5, 0.75, 1, 1.25, 1.5, 1.75, 2, 2.5, 3 ],
 
       plugins,
 
