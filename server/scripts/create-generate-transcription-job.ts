ebfcb61a61a51ab65a495a46459b9adf7d02dc83
Chocobozzz <me@florianbigard.com>
2026-02-26 15:50:18 +0100

    Add generate transcription script
    It's undocumented and untested because we plan to move it in the web
    interface in the following months

--- server/scripts/create-generate-transcription-job.ts
+++ server/scripts/create-generate-transcription-job.ts
@@ -1,21 +1,21 @@
 import { toCompleteUUID } from '@server/helpers/custom-validators/misc.js'
 import { initDatabaseModels } from '@server/initializers/database.js'
 import { JobQueue } from '@server/lib/job-queue/index.js'
-import { addLocalOrRemoteStoryboardJobIfNeeded } from '@server/lib/video-jobs.js'
-import { StoryboardModel } from '@server/models/video/storyboard.js'
+import { createTranscriptionTaskIfNeeded } from '@server/lib/video-captions.js'
+import { VideoCaptionModel } from '@server/models/video/video-caption.js'
 import { VideoModel } from '@server/models/video/video.js'
 import { program } from 'commander'
 
 program
-  .description('Generate videos storyboard')
-  .option('-v, --video [videoUUID]', 'Generate the storyboard of a specific video')
-  .option('-a, --all-videos', 'Generate missing storyboards of local videos')
+  .description('Generate videos transcription')
+  .option('-v, --video [videoUUID]', 'Generate the transcription of a specific video')
+  .option('-a, --all-videos', 'Generate missing transcriptions of local videos')
   .parse(process.argv)
 
 const options = program.opts()
 
 if (!options['video'] && !options['allVideos']) {
-  console.error('You need to choose videos for storyboard generation.')
+  console.error('You need to choose videos for transcription generation.')
   process.exit(-1)
 }
 
@@ -53,7 +53,7 @@ async function run () {
 
     ids.push(video.id)
   } else {
-    ids = await listLocalMissingStoryboards()
+    ids = await listLocalMissingTranscriptions()
   }
 
   for (const id of ids) {
@@ -61,22 +61,22 @@ async function run () {
     if (!videoFull || videoFull.isLive) continue
 
     try {
-      await addLocalOrRemoteStoryboardJobIfNeeded({ video: videoFull, federate: true })
+      await createTranscriptionTaskIfNeeded(videoFull)
 
-      console.log(`Created generate-storyboard job for ${videoFull.name}.`)
+      console.log(`Created generate-transcription job for ${videoFull.name}.`)
     } catch (err) {
-      console.error(`Error while creating generate-storyboard job for video ${videoFull.name}:`, err)
+      console.error(`Error while creating generate-transcription job for video ${videoFull.name}:`, err)
     }
   }
 }
 
-async function listLocalMissingStoryboards () {
+async function listLocalMissingTranscriptions () {
   const ids = await VideoModel.listLocalIds()
   const results: number[] = []
 
   for (const id of ids) {
-    const storyboard = await StoryboardModel.loadByVideo(id)
-    if (!storyboard) results.push(id)
+    const captions = await VideoCaptionModel.listVideoCaptions(id)
+    if (captions.length === 0) results.push(id)
   }
 
   return results
