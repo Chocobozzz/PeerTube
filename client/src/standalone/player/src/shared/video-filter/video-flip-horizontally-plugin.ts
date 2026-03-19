d7af66b96d6c3bfa85a4a251dcabfa18ba96b1c7
sunny-codex <265379505+sunny-codex@users.noreply.github.com>
2026-03-19 16:12:45 -0700

    Add cleanup method for VideoFlipHorizontallyPlugin

--- client/src/standalone/player/src/shared/video-filter/video-flip-horizontally-plugin.ts
+++ client/src/standalone/player/src/shared/video-filter/video-flip-horizontally-plugin.ts
@@ -4,12 +4,15 @@ import { VideojsPlayer, VideojsPlugin} from '../../types'
 const Plugin = videojs.getPlugin('plugin') as typeof VideojsPlugin;
 
 class VideoFlipHorizontallyPlugin extends Plugin {
-
   constructor(player: VideojsPlayer ){
     super(player);
     player.on('video-flip-horizontally-toggle', () => {
       this.player.toggleClass('vjs-vid-flip-h')
     })
+  }
+  dispose(): void {
+    this.player.removeClass('vjs-vid-flip-h')
+    super.dispose();
   }
 }
 
