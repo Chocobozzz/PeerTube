acaafd36e0f6ce3f07b867155a2f9d51e18bca2a
sunny-codex <265379505+sunny-codex@users.noreply.github.com>
2026-03-20 11:50:19 -0700

    Add menu to video-filter-menu-button

--- client/src/standalone/player/src/shared/settings/video-filter-menu-button.ts
+++ client/src/standalone/player/src/shared/settings/video-filter-menu-button.ts
@@ -1,5 +1,6 @@
 import videojs from "video.js"
 import { VideojsButtonOptions, VideojsMenuButton, VideojsPlayer } from "../../types"
+import Menu from "video.js/dist/types/menu/menu"
 
 const MenuButton = videojs.getComponent('MenuButton') as typeof VideojsMenuButton
 
@@ -7,6 +8,11 @@ class VideoFilterMenuButton extends MenuButton {
   constructor(player: VideojsPlayer, options: VideojsButtonOptions) {
     super(player, options)
     this.controlText("Video Filter")
+  }
+
+  createMenu(): Menu {
+    const menu = new Menu(this.player_, { menuButton: this })
+    return menu
   }
 
 }
