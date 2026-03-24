559c60033779d4887fb31da01f6e65afd2a0c29b
sunny-codex <265379505+sunny-codex@users.noreply.github.com>
2026-03-24 13:05:26 -0700

    Rewrite event to be more explicit

--- client/src/standalone/player/src/shared/video-filter/video-flip-horizontally-plugin.ts
+++ client/src/standalone/player/src/shared/video-filter/video-flip-horizontally-plugin.ts
@@ -6,8 +6,11 @@ const Plugin = videojs.getPlugin('plugin') as typeof VideojsPlugin
 class VideoFlipHorizontallyPlugin extends Plugin {
   constructor(player: VideojsPlayer) {
     super(player)
-    player.on('video-flip-horizontally-toggle', () => {
-      this.player.toggleClass('vjs-vid-flip-h')
+    player.on('video-flip-horizontally-on', () => {
+      this.player.addClass('vjs-vid-flip-h')
+    })
+    player.on('video-flip-horizontally-off', () => {
+      this.player.removeClass(('vjs-vid-flip-h'))
     })
   }
   dispose(): void {
