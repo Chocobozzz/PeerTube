4790f94b9d80889131449f5c318fe03c0f13d1cf
Chocobozzz <me@florianbigard.com>
2026-06-18 10:09:19 +0200

    Fix migrations conflict

--- server/core/initializers/constants.ts
+++ server/core/initializers/constants.ts
@@ -62,7 +62,7 @@ import { CONFIG, registerConfigChangedHandler } from './config.js'
 
 // ---------------------------------------------------------------------------
 
-export const LAST_MIGRATION_VERSION = 1055
+export const LAST_MIGRATION_VERSION = 1060
 
 // ---------------------------------------------------------------------------
 
