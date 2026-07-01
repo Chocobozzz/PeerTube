6d6a60165b55d6239a3fc8f8327ba4c4c0c42a2a
Chocobozzz <me@florianbigard.com>
2026-07-01 11:25:15 +0200

    SQL injection hardening

--- server/core/models/video/sql/video/videos-id-list-query-builder.ts
+++ server/core/models/video/sql/video/videos-id-list-query-builder.ts
@@ -10,7 +10,7 @@ import {
 } from '@peertube/peertube-models'
 import { exists } from '@server/helpers/custom-validators/misc.js'
 import { WEBSERVER } from '@server/initializers/constants.js'
-import { buildSortDirectionAndField } from '@server/models/shared/index.js'
+import { buildSortDirectionAndField, throwOnInvalidSortColumnName } from '@server/models/shared/index.js'
 import { MUserAccountId, MUserId } from '@server/types/models/index.js'
 import { Transaction } from 'sequelize'
 import validator from 'validator'
@@ -1020,7 +1020,7 @@ export class VideosIdListQueryBuilder extends AbstractRunQuery {
   }
 
   private buildOrder (column: string, direction: 'ASC' | 'DESC') {
-    if (column.match(/^[a-zA-Z."]+$/) === null) throw new Error('Invalid sort column ' + column)
+    throwOnInvalidSortColumnName(column)
 
     if (column === 'random') return 'ORDER BY RANDOM()'
     if (column === 'total') return `ORDER BY "total" ${direction}`
