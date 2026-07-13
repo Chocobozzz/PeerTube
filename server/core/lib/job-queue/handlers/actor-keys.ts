172304686dbc7d5b550c982ed15c2a0042e48eaa
Chocobozzz <me@florianbigard.com>
2026-07-13 15:56:37 +0200

    More robust actor keys creation

--- server/core/lib/job-queue/handlers/actor-keys.ts
+++ server/core/lib/job-queue/handlers/actor-keys.ts
@@ -4,17 +4,11 @@ import { ActorModel } from '@server/models/actor/actor.js'
 import { ActorKeysPayload } from '@peertube/peertube-models'
 import { logger } from '../../../helpers/logger.js'
 
-async function processActorKeys (job: Job) {
+export async function processActorKeys (job: Job) {
   const payload = job.data as ActorKeysPayload
   logger.info('Processing actor keys in job %s.', job.id)
 
   const actor = await ActorModel.load(payload.actorId)
 
   await generateAndSaveActorKeys(actor)
-}
-
-// ---------------------------------------------------------------------------
-
-export {
-  processActorKeys
 }
